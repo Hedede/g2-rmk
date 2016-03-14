8ad4f8dbb5b9d8f841f73359c8997029c95f8def
Hudd <hededrk@gmail.com>
2016-03-14 16:43:19 +0300

    Decompile parts of zCArchiverGeneric

--- decomp/zCObject/zArchiverGeneric.cpp
+++ decomp/zCObject/zArchiverGeneric.cpp
@@ -1,7 +1,416 @@
+zCArchiverGeneric::~zCArchiverGeneric()
+{
+	Close();
+	writeObjectEntries.Clear();
+}
+
+void zCArchiverGeneric::RestoreBuffer(void* out, size_t size)
+{
+	if ( useBuffer )
+		buffer->Read(out, outsize);
+	else
+		file->Read(out, outsize);
+}
+
+void zCArchiverGeneric::RestoreString0(zSTRING& out)
+{
+	RestoreStringEOL(out);
+}
+
+int zCArchiverGeneric::RestoreGetPos()
+{
+	if ( useBuffer )
+		return buffer->GetPos();
+	return file->Pos();
+}
+
+int32_t zCArchvierGeneric::ReadInt(char const* name)
+{
+	int tmp;
+	ReadInt(name, tmp);
+	return tmp;
+}
+
+void zCArchvierGeneric::ReadInt(char const* name,int& value)
+{
+	if ( inBinaryMode ) {
+		RestoreBuffer(value, sizeof(value));
+		return;
+	}
+
+	if (ReadEntryASCII(name, tempString))
+		value = tempString.ToInt();
+}
+
+uint8_t zCArchvierGeneric::ReadByte(char const* name)
+{
+	uint8_t tmp;
+	ReadByte(name, tmp);
+	return tmp;
+}
+
+void zCArchvierGeneric::ReadByte(char const* name, uint8_t& byte)
+{
+	if ( inBinaryMode ) {
+		RestoreBuffer(value, sizeof(byte));
+		return;
+	}
+
+	if (ReadEntryASCII(name, tempString))
+		value = tempString.ToInt();
+}
+
+uint16_t zCArchvierGeneric::ReadWord(char const* name)
+{
+	uint16_t tmp;
+	ReadWord(name, tmp);
+	return tmp;
+}
+void zCArchvierGeneric::ReadWord(char const* name,uint16_t& word)
+{
+	if ( inBinaryMode ) {
+		RestoreBuffer(value, sizeof(word));
+		return;
+	}
+
+	if (ReadEntryASCII(name, tempString))
+		value = tempString.ToInt();
+}
+
+void zCArchvierGeneric::ReadFloat(char const* name)
+{
+	float tmp;
+	ReadFloat(name, tmp);
+	return tmp;
+}
+
+void zCArchvierGeneric::ReadFloat(char const* name, float& value)
+{
+	if ( inBinaryMode ) {
+		RestoreBuffer(value, sizeof(value));
+		return;
+	}
+
+	if (ReadEntryASCII(name, tempString))
+		value = tempString.ToFloat();
+}
+
+bool zCArchvierGeneric::ReadBool(char const* name)
+{
+	zBOOL val;
+	ReadBool(name, val);
+	return val;
+}
+
+void zCArchvierGeneric::ReadBool(char const* name, zBOOL& val)
+{
+	if ( inBinaryMode ) {
+		val = ReadByte(name);
+		return;
+	}
+
+	if (ReadEntryASCII(name, tempString))
+		value = tempString.ToInt();
+}
+
+zSTRING zCArchvierGeneric::ReadString(char const* name)
+{
+	zSTRING str;
+	ReadString(name, str);
+	return str;
+}
+void zCArchvierGeneric::ReadString(char const* name, zSTRING& str)
+{
+	if ( inBinaryMode )
+		RestoreString0(str);
+	else
+		ReadEntryASCII(name, str);
+}
+
+zVEC3 zCArchvierGeneric::ReadVec3(char const* name)
+{
+	zVEC3 vec;
+	ReadVec3(name, vec);
+	return vec;
+}
+void zCArchvierGeneric::ReadVec3(char const* name,zVEC3& vec)
+{
+	if ( inBinaryMode ) {
+		enu = ReadByte(name);
+		return;
+	}
+
+	if (ReadEntryASCII(name, tempString)) {
+		float r, g, b, a;
+		sscanf(tempstr.Data(), "%f %f %f", &x, &y, &z);
+	}
+}
+
+zCOLOR zCArchvierGeneric::ReadColor(char const* name)
+{
+	zCOLOR color;
+	ReadColor(name, color);
+	return color;
+}
+
+void zCArchvierGeneric::ReadColor(char const* name, zCOLOR& color)
+{
+	if ( inBinaryMode ) {
+		enu = ReadByte(name);
+		return;
+	}
+
+	if (ReadEntryASCII(name, tempString)) {
+		uint32_t r, g, b, a;
+		sscanf(tempstr.Data(), "%d %d %d %d", &r, &g, &b, &a);
+
+		color[2] = r;
+		color[1] = g;
+		color[0] = b;
+		color[3] = a;
+	}
+}
+
+uint32_t zCArchvierGeneric::ReadEnum(char const* name)
+{
+	uint32_t enu;
+	ReadEnum(name, enu);
+	return enu;
+}
+
+void zCArchvierGeneric::ReadEnum(char const* name, ulong enu)
+{
+	if ( inBinaryMode ) {
+		enu = ReadByte(name);
+		return;
+	}
+
+	if (ReadEntryASCII(name, tempString))
+		enu = tempString.ToInt();
+}
+
+virtual void ReadRaw(char const* name, void* out, size_t size)
+{
+	if ( inBinaryMode ) {
+		RestoreBuffer(out, size);
+		return;
+	}
+
+	ReadEntryASCII(name, tempString);
+	size_t avail = tempString.Length() / 2;
+	if ( size != avail )
+		zFAULT("D: zArchiver(zCArchiverGeneric::ReadRaw): size of requested data does not equal size of available data: " + size + "/" + avail); // 1468
+
+	char* output = static_cast<char*>(out);
+	char const* data = tempString.Data();
+	while (avail) {
+		uint8_t byte;
+		sscanf(data, "%2x", &byte);
+
+		*output++ = byte;
+
+		data += 2;
+
+		--avail;
+	}
+}
+
+virtual void ReadRawFloat(char const* name, void* out, size_t size)
+{
+	if ( inBinaryMode )
+	{
+		RestoreBuffer(out, size);
+		return;
+	}
+
+	ReadEntryASCII(name, tempString);
+
+	size_t num = size / 4;
+	for (size_t i = 0; i < num; ++i) {
+		zSTRING word = tempString.PickWord(i, " ", " ");
+		*out++ = word.ToFloat();
+	}
+}
+
+void zCArchiverGeneric::StoreBuffer(void* buf, size_t size)
+{
+	if ( size ) {
+		if ( useBuffer )
+			buffer->Write(buf, size);
+		else
+			file->Write(buf, size);
+	}
+}
+
+void zCArchiverGeneric::StoreString(const char* str)
+{
+	size_t len = strlen(str);
+	StoreBuffer(str, len);
+}
+
+void zCArchiverGeneric::StoreStringEOL(const char *a2, const char *a3)
+{
+	StoreString(str);
+	StoreString(stringEOL.Data());
+}
+
+void zCArchiverGeneric::WriteInt(const char *name, int32_t value)
+{
+	if ( inBinaryMode ) {
+		StoreBuffer(&value, sizeof(value));
+		return;
+	}
+
+	WriteASCIILine(name, "int", zSTRING(value));
+}
+
+void zCArchiverGeneric::WriteWord(char const* name, uint16_t word)
+{
+	if ( inBinaryMode ) {
+		StoreBuffer(&word, sizeof(word));
+		return;
+	}
+	WriteASCIILine(name, "int", zSTRING(word));
+}
+
+void zCArchiverGeneric::WriteByte(const char* name, uint8_t value)
+{
+	if ( inBinaryMode ) {
+		StoreBuffer(&value, sizeof(value));
+		return;
+	}
+
+	WriteASCIILine(name, "int", zSTRING(value));
+}
+
+void zCArchiverGeneric::WriteBool(const char* name, uint8_t value)
+{
+	if ( inBinaryMode ) {
+		StoreBuffer(&value, sizeof(value));
+		return;
+	}
+
+	WriteASCIILine(name, "bool", zSTRING(value));
+}
+
+void zCArchiverGeneric::WriteWord(char const* name, zCOLOR const& value)
+{
+	if ( inBinaryMode ) {
+		StoreBuffer(&word, sizeof(value));
+		return;
+	}
+	WriteASCIILine(name, "color", value.GetDescription());
+}
+
+void zCArchiverGeneric::WriteEnum(const char *name, const char *enumChoices, int value)
+{
+	if ( inBinaryMode ) {
+		WriteByte(name, value);
+		return;
+	}
+
+	if ( InProperties() ) {
+		WriteASCIILine(name, "enum;" + enumChoices, zSTRING(value));
+		return;
+	}
+
+	WriteASCIILine(name, "enum", zSTRING(value));
+}
+
+void zCArchiverGeneric::WriteFloat(const char *name, float value)
+{
+	if ( inBinaryMode ) {
+		WriteByte(name, value);
+		return;
+	}
+
+	WriteASCIILine(name, "float", zSTRING(value));
+}
+
+
+void zCArchiverGeneric::WriteVec3(char const* name, zVEC3 const& value)
+{
+	if ( inBinaryMode ) {
+		StoreBuffer(&value, sizeof(value));
+		return;
+	}
+	WriteASCIILine(name, "vec3", value.GetDescription());
+}
+
+void zCArchiverGeneric::WriteString(const char *name, zSTRING const& value)
+{
+	if ( inBinaryMode ) {
+		StoreString(value.Data());
+		StoreBuffer(byte_8D482C, 1);// I?
+		return;
+	}
+
+	WriteASCIILine(name, "string", value);
+}
+
+void zCArchiverGeneric::WriteRaw(const char* name, void* val, size_t size)
+{
+	if ( inBinaryMode ) {
+		StoreBuffer(val, size);
+		return;
+	}
+
+	zSTRING raw;
+	char* ptr = static_cast<char*>(val);
+	for (size_t s = 0; s < size; ++s) {
+		char buf[3] = {};
+		sprintf(buf, "%02x", ptr[s]);
+		raw += buf;
+	}
+
+	WriteASCIILine(name, "raw", raw);
+}
+
+void zCArchiverGeneric::WriteRawFloat(const char* name, void* src, size_t size)
+{
+	if ( size & 3 ) {
+		zFATAL("D: zArchiver(zCArchiverGeneric::WriteRawFloat): raw float buffer has illegal size (mod4!=0): "_s + size); // 557
+	}
+
+	if ( inBinaryMode ) {
+		StoreBuffer(val, size);
+		return;
+	}
+
+	float* floats = static_cast<float*>(src);
+	zSTRING raw;
+	size_t count = size / 4;
+	for (size_t i; i < count; ++i )
+		raw += zSTRING(floats[i]);
+
+	WriteASCIILine(name, "rawFloat", raw);
+}
+
+
+char const* spaces = "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t";
+
+void zCArchiverGeneric::WriteASCIILine(const char *name, const char *type, zSTRING const& value)
+{
+	if ( !IsStringValid(name)  ||
+	     !IsStringValid(value) )
+	{
+		zERROR::Fault("illegal characters: \"" + name + " / " + value);
+	}
+
+	StoreBuffer(spaces, chunkRecords.GetNumInList());
+	StoreString(name);
+	StoreBuffer(" =", 1);
+	StoreString(type);
+	StoreBuffer(" :", 1);
+	StoreStringEOL(value);
+}
+
+
+
 void zCArchiverGeneric::CheckObjectListSize(int numRequired)
 {
-	if ( numRequired > objects.GetNumInList() )
-		objects.Resize(numRequired + numRequired / 2);
+	if ( numRequired > objectList.GetNumInList() )
+		objectList.Resize(numRequired + numRequired / 2);
 }
 
 zCObject* zCArchiverGeneric::ReadObject(const char *name, zCObject* object)
@@ -19,19 +428,19 @@ zCObject* zCArchiverGeneric::ReadObject(const char *name, zCObject* object)
 	if (chunkType[0] == '§') {
 		int index = chunkRecords.End().objRef;
 
-		if ( index >= objects.GetNumInList() )
+		if ( index >= objectList.GetNumInList() )
 			zFATAL("D: zArchiver(::ReadObject): objRef, illegal object index: "_s + objRef); // 1167
 
-		if (!objects[index])
+		if (!objectList[index])
 			zFATAL("D: zArchiver(::ReadObject): objRef, referenced object has not been read, object index: "_s + index); // 1168
 
 		if (!object) {
-			objects[index]->AddRef();
+			objectList[index]->AddRef();
 			SkipChunk(1);
-			return objects[index];
+			return objectList[index];
 		}
 
-		if ( objects[index] != object )
+		if ( objectList[index] != object )
 			zWARNING("D: zArchiver(::ReadObject): found an objectPtr in file, tried to write this to an 'objectUseThis'"); // 1176
 
 		SkipChunk(1);
@@ -58,10 +467,10 @@ zCObject* zCArchiverGeneric::ReadObject(const char *name, zCObject* object)
 
 				CheckObjectListSize(index + 1);
 
-				zASSERT(!objects[index], "Archiver tried to overwrite object in its list. Did you try to load an incompatible savegame?"); // 1232
+				zASSERT(!objectList[index], "Archiver tried to overwrite object in its list. Did you try to load an incompatible savegame?"); // 1232
 
 				object->AddRef();
-				objects[index] = object;
+				objectList[index] = object;
 			}
 		}
 
@@ -75,7 +484,7 @@ zCObject* zCArchiverGeneric::ReadObject(const char *name, zCObject* object)
 
 	auto classDef = retObj->_GetClassDef();
 	uint16_t version = classDef->versionSum;
-	uint16_t chunkVersion = chunkRecords.End().versionSum;
+	uint16_t chunkVersion = GetCurrentChunkVersion();
 
 	if (chunkVersion != version && inBinaryMode) {
 		zWARNING("D: Archiver(::ReadObject): skipping object-chunk, tried to read object-chunk with out-of-date version: " + chunkVersion); // 1253
@@ -85,12 +494,12 @@ zCObject* zCArchiverGeneric::ReadObject(const char *name, zCObject* object)
 
 		CheckObjectListSize(index + 1);
 
-		zASSERT(!objects[index] || !index, "Archiver tried to overwrite object in its list. Did you try to load an incompatible savegame?"); // 1269
+		zASSERT(!objectList[index] || !index, "Archiver tried to overwrite object in its list. Did you try to load an incompatible savegame?"); // 1269
 
-		Release(objects[index]);
+		Release(objectList[index]);
 
-		objects[index] = retObj;
-		objects[index]->Unarchive(*this);
+		objectList[index] = retObj;
+		objectList[index]->Unarchive(*this);
 		ReadChunkEnd();
 		return retObj;
 	}
@@ -129,8 +538,8 @@ void zCArchiverGeneric::WriteObject(const char *name, zCObject& obj)
 		obj.AddRef();
 
 
-		int index = objects.GetNumInList();
-		objects.Insert(obj);
+		int index = objectList.GetNumInList();
+		objectList.Insert(obj);
 
 		auto entry = writeObjectEntries.Insert(&obj);
 
@@ -161,32 +570,6 @@ void zCArchiverGeneric::WriteObject(const char* name, zCObject* obj)
 	}
 
 	WriteObject(name, *obj);
-}
-
-void zCArchiverGeneric::WriteWord(char const* name, uint16_t word)
-{
-	if ( this->unk_1[0] )
-	{
-		StoreBuffer(&word, sizeof(word));
-		return;
-	}
-	vtable->WriteASCIILine(name, "int", zSTRING(word));
-}
-
-void zCArchiverGeneric::WriteASCIILine(const char *name, const char *type, zSTRING const& value)
-{
-	if ( !IsStringValid(name)  ||
-	     !IsStringValid(value) )
-	{
-		zERROR::Fault("illegal characters: \"" + name + " / " + value);
-	}
-
-	StoreBuffer(zTabs, this->indentLevel);// string with lotsa tabs
-	StoreString(name);
-	StoreBuffer(" =", 1);
-	StoreString(type);
-	StoreBuffer(" :", 1);
-	StoreStringEOL(value);
 }
 
 uint16_t zCArchiverGeneric::GetCurrentChunkVersion()
--- decomp/zCObject/zCArchiverGeneric.h
+++ decomp/zCObject/zCArchiverGeneric.h
@@ -11,25 +11,23 @@ public:
 		}
 		return true;
 	}
-
-	static zSTRING GetStringEOL()
-	{
-		return stringEOL;
-	}
-
 public:
+	zCArchiverGeneric();
 	virtual ~zCArchiverGeneric();
+
 	virtual void WriteInt(char const *,int);
 	virtual void WriteByte(char const *,uchar);
 	virtual void WriteWord(char const *,ushort);
 	virtual void WriteFloat(char const *,float);
-	virtual void WriteBool(char const *,int);
+	virtual void WriteBool(char const *, uint8_t);
 	virtual void WriteString(char const *,zSTRING const &);
 	virtual void WriteVec3(char	const *,zVEC3 const &);
-	virtual void WriteColor(char const *,zCOLOR const	&);
+	virtual void WriteColor(char const *,zCOLOR const&);
+
+	virtual void WriteEnum(char	const *,char const *,int);
+
 	virtual void WriteRaw(char	const *,void *,ulong);
 	virtual void WriteRawFloat(char const	*,void *,ulong);
-	virtual void WriteEnum(char	const *,char const *,int);
 
 	virtual void WriteObject(char const* name, zCObject& obj);
 	virtual void WriteObject(char const* name, zCObject* obj);
@@ -43,26 +41,36 @@ public:
 	virtual void WriteChunkEnd();
 	virtual void WriteGroupBegin(char const	*);
 	virtual void WriteGroupEnd(char const *);
-	virtual void ReadInt(char const	*);
-	virtual void ReadInt(char const *,int &);
-	virtual void ReadByte(char const *);
-	virtual void ReadByte(char const *,uchar &);
-	virtual void ReadWord(char const *);
-	virtual void ReadWord(char const *,ushort &);
-	virtual void ReadFloat(char const *);
-	virtual void ReadFloat(char const *,float &);
-	virtual void ReadBool(char const *);
-	virtual void ReadBool(char const *,int &);
-	virtual void ReadString(char const *);
-	virtual void ReadString(char const *,zSTRING &);
-	virtual void ReadVec3(char const *);
-	virtual void ReadVec3(char const *,zVEC3 &);
-	virtual void ReadColor(char	const *);
-	virtual void ReadColor(char const *,zCOLOR	&);
-	virtual void ReadEnum(char const *);
-	virtual void ReadEnum(char const *,int &);
-	virtual void ReadRaw(char const *,void *,ulong);
-	virtual void ReadRawFloat(char	const *,void *,ulong);
+
+	virtual int32_t  ReadInt(char const* name);
+	virtual void     ReadInt(char const* name,int& value);
+
+	virtual uint8_t  ReadByte(char const* name);
+	virtual void     ReadByte(char const* name, uint8_t& byte);
+
+	virtual uint16_t ReadWord(char const* name);
+	virtual void     ReadWord(char const* name,uint16_t& word);
+
+	virtual float    ReadFloat(char const* name);
+	virtual void     ReadFloat(char const* name, float& value);
+
+	virtual bool     ReadBool(char const* name);
+	virtual void     ReadBool(char const* name,zBOOL& val);
+
+	virtual zSTRING  ReadString(char const* name);
+	virtual void     ReadString(char const* name, zSTRING& str);
+
+	virtual zVEC3    ReadVec3(char const* name);
+	virtual void     ReadVec3(char const* name,zVEC3& vec);
+
+	virtual zCOLOR   ReadColor(char const* name);
+	virtual void     ReadColor(char const* name,zCOLOR& color);
+
+	virtual uint32_t ReadEnum(char const* name);
+	virtual void     ReadEnum(char const* name, ulong& enu);
+
+	virtual void     ReadRaw(char const* name, void *, size_t size);
+	virtual void     ReadRawFloat(char const* name,void *, size_t size);
 
 	virtual void ReadObject(char const* name, zCObject* obj);
 	virtual void ReadObject(zCObject* obj)
@@ -73,15 +81,26 @@ public:
 	virtual void ReadChunkStart(char const *);
 	virtual void ReadChunkStart(zSTRING&, ushort&);
 	virtual void ReadChunkStartNamed(char const *,ushort &);
+
 	virtual void SkipOpenChunk();
 	virtual void GetCurrentChunkVersion();
+
 	virtual zFILE* GetFile();
 	virtual void GetBufferString(zSTRING &);
 	virtual void GetBuffer();
 	virtual void EndOfArchive();
 	virtual void Close();
-	virtual void SetStringEOL(zSTRING const &);
-	virtual void GetStringEOL();
+
+	virtual void SetStringEOL(zSTRING const& eol)
+	{
+		stringEOL = eol;
+	}
+
+	virtual void GetStringEOL() const
+	{
+		return stringEOL;
+	}
+
 	virtual bool IsStringValid(char const* str)
 	{
 		return IsStringValid(str);
@@ -98,32 +117,48 @@ public:
 	{
 		noReadSearchCycles = val;
 	}
-	virtual void InProperties();
+
+	virtual bool InProperties() const
+	{
+		return this->mode == ARC_MODE_ASCII_PROPS;
+	}
+
 	virtual bool InSaveGame() const
 	{
 		return inSaveGame;
 	}
-	virtual void InBinaryMode();
+
+	virtual void InBinaryMode()
+	{
+		return mode == ARC_MODE_BINARY || mode == ARC_MODE_BIN_SAFE;
+	}
+
 	virtual void GetParentObject();
 	virtual void OpenWriteBuffer(zCBuffer *,zTArchiveMode,int,int,int);
 	virtual void OpenWriteFile(zFILE *,zTArchiveMode,int,int,int);
+
 	virtual void WriteHeader(int);
 	virtual void WriteHeaderNumObj();
-	virtual void WriteASCIILine(char const *,char const	*,zSTRING const	&);
-	virtual void StoreBuffer(void *,ulong);
-	virtual void StoreString(char const	*);
-	virtual void StoreStringEOL(char	const *);
+	virtual void WriteASCIILine(char const *,char const*,zSTRING const&);
+
+	virtual void StoreBuffer(void*, ulong);
+	virtual void StoreString(char const*);
+	virtual void StoreStringEOL(char const *);
 	virtual void StoreGetPos();
 	virtual void StoreSeek(ulong);
+
 	virtual void OpenReadBuffer(zCBuffer &,zTArchiveMode,int,int);
 	virtual void OpenReadFile(zFILE	*,zTArchiveMode,int,int,int);
+
 	virtual void GetClassDefByString(zSTRING const &);
 	virtual void CreateObject(zSTRING const &);
+
 	virtual void SkipChunk(int);
 	virtual void ReadChunkStartASCII(char const *,zSTRING &);
 	virtual void ReadChunkEnd();
 	virtual void ReadEntryASCII(char const *,zSTRING &);
 	virtual void ReadHeader();
+
 	virtual void RestoreBuffer(void *,ulong);
 	virtual void RestoreStringEOL(zSTRING	&);
 	virtual void RestoreString0(zSTRING &);
@@ -134,4 +169,33 @@ private:
 	int DebugMessage(zSTRING const& msg);
 	void CheckObjectListSize(int numRequired);
 
+private:
+	zFILE*    file   = nullptr;
+	zCBuffer* buffer = nullptr;
+
+	zBOOL arcOwnsMedium = 0;
+	zBOOL inSaveGame    = 0;
+	zBOOL inBinaryMode  = 0;
+	zBOOL rwMode        = 0;
+
+	zCArray<zTChunkRecord> chunkRecords;
+	zCArray<zCObject*> objectList;
+
+
+	int __maybe_headerstart;
+	int unk_2;
+	int __matbe_headerend;
+
+	int checksumEnabled    = 0;
+	int arcFlags           = 0;
+	int notInProperties    = 0;
+	int noReadSearchCycles = 0;
+
+	zSTRING tempString;
+
+	int unk_1;
+
+	zCSparseArray<zCObject*,zTWriteObjectEntry> writeObjectEntries;
+
+	int unk_6[3];
 };

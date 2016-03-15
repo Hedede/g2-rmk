55eb2b16c1ff5fc22a8836193ba69fcbba68ad34
Hudd <hededrk@gmail.com>
2016-03-15 04:58:24 +0300

    Decompile some zOptions

--- decomp/Core/zCOption.cpp
+++ decomp/Core/zCOption.cpp
@@ -1,8 +1,3 @@
-//#################################################################
-//
-//  Ini File Interface: zCOption
-//
-//#################################################################
 /*
    The sense of "Temporary values" is probably, that the developers
    thought of an "apply" button. If that "apply" button is used,
@@ -10,10 +5,6 @@
 
    Doesn't seem like this feature is used anywhere.
  */
-//************************************
-//  zCOptions is the interface
-//  to access ini files.
-//************************************
 
 zSTRING[] zOPTION_PATHNAMES = {
 	"DIR_SYSTEM", "DIR_WEB", "DIR_SAVEGAMES", "DIR_DATA", "DIR_ANIMS",
@@ -24,7 +15,7 @@ zSTRING[] zOPTION_PATHNAMES = {
 	"DIR_TOOLS_CONFIG", "SUBDIR_INTERN", "", "DIR_ROOT", "DIR_EXECUTABLE",
 }
 
-enum zOPTION_PATH {
+enum zTOptionPaths {
 	DIR_SYSTEM,
 	DIR_WEB,
 	DIR_SAVEGAMES,
@@ -79,6 +70,20 @@ class zCOption {
 public:
 	virtual ~zCOption();
 
+	void AddParameters(zSTRING params);
+	void ChangeDir(zTOptionPaths dirId);
+
+	zSTRING& zCOption::GetDirString(zTOptionPaths dirId)
+	{
+		return dir_string[dirId];
+	}
+
+	uint32_t zCOption::GetNumSections() const
+	{
+		return sectionList.GetNumInList();
+	}
+
+	int Parm(zSTRING const& parm);
 	int ReadRaw(zSTRING const& secName, char const* optName, char *& outArray,
 	        char* orArray, int orArraySize);
 private:
@@ -92,6 +97,202 @@ private:
 	zSTRING commandline;
 };
 
+void zDeinitOptions();
+
+void zInitOptions()
+{
+	if ( zoptions )
+		return;
+
+	zoptions = new zCOption();
+
+	zoptions->Init(sysCommandLine, 1);
+	zoptions->ChangeDir(DIR_SYSTEM);
+
+	zSTRING ini = zoptions->ParmValue("ini");
+
+	if (ini.IsEmpty())
+		ini = "Gothic.ini";
+
+	zoptions->Load(ini);
+
+	int version = zoptions->ReadInt(zOPT_SEC_GAME, "PATCHVERSION", 0);
+	if (version < 0 )
+		version = 0;
+
+	switch (version) {
+	case 0:
+		zoptions->WriteBool(zOPT_SEC_GAME, "enableJoystick", 0, 0);
+		++version;
+	case 1:
+		zoptions->WriteReal(zOPT_SEC_GAME, "SHORTKEY1FARPLANEDIST", 0.8, 0);
+		zoptions->WriteReal(zOPT_SEC_GAME, "SHORTKEY2FARPLANEDIST", 1.2, 0);
+		zoptions->WriteReal(zOPT_SEC_GAME, "SHORTKEY3FARPLANEDIST", 2.0, 0);
+		zoptions->WriteReal(zOPT_SEC_GAME, "SHORTKEY4FARPLANEDIST", 3.0, 0);
+		zoptions->WriteBool("RENDERER_D3D", "zSyncAmbientCol", 0, 0);
+
+		zoptions->WriteBool(zOPT_SEC_INTERNAL, "logStatistics", 0, 0);
+		zoptions->WriteBool(zOPT_SEC_INTERNAL, "extendedMenu", 0, 0);
+		zoptions->WriteBool(zOPT_SEC_INTERNAL, "gameAbnormalExit", 0, 0);
+
+		zoptions->WriteReal("ENGINE", "zInventoryItemsDistanceScale", 1.3, 0);
+		++version;
+	case 2:
+		zoptions->WriteBool("RENDERER_D3D", "radeonHackAmbientColBug", 0, 0);
+		++version;
+	case 3:
+		zoptions->WriteString(zOPT_SEC_INTERNAL, "gameScript", "", 0);
+		zoptions->WriteString(zOPT_SEC_INTERNAL, "playerInstanceName", "", 0);
+		++version;
+	case 4:
+		zoptions->WriteBool("RENDERER_D3D", "geforce3HackWBufferBug", 1, 0);
+		zoptions->WriteBool("ENGINE", "zSkyRenderFirst", 1, 0);
+		++version;
+	default:
+		break;
+	}
+
+	zoptions->WriteInt("GAME", "PATCHVERSION", version, 0);
+
+	if (zoptions->ReadBool(zOPT_SEC_INTERNAL, "gameStartFailed", 0))
+		zWARNING("B: GOTHIC: Start of gothic failed before."); // zzBert.cpp 146
+
+	if (zoptions->ReadBool(zOPT_SEC_INTERNAL, "gameAbnormalExit", 0)) {
+		zWARNING("B: GOTHIC: Abnormal exit of gothic detected."); // 152
+		zWARNING("N: Force Initializing VDFS after Crash."); // 163
+
+		bool devmode = zoptions->Parm("DEVMODE");
+		if ( devmode ) {
+			zWARNING("N: Forced Initialization of VDFS canceled (-devmode)") // 165
+		} else {
+			char filename[260];
+			memset(filename, 0, 260);
+
+			GetModuleFileNameA(0, filename, 0x104u);
+
+			auto i = &filename[lstrlenA(filename)];
+			for (; i >= filename ; --i) {
+				if (*i == '\\')
+					break;
+				if (*i == '/')
+					break;
+			}
+			if (i > filename) {
+				*i-- = 0;
+				for (; i >= filename ; *i-- = 0) {
+					if (*i == '\\')
+						break;
+					if (*i == '/')
+						break;
+					
+				}
+				lstrcatA(filename, "VDFS.DMP");
+				DeleteFileA(filename);
+			}
+		}
+	}
+
+	zoptions->WriteBool(zOPT_SEC_INTERNAL, "gameAbnormalExit", 1, 0);
+	zoptions->WriteBool(zOPT_SEC_INTERNAL, "gameStartFailed", 1, 0);
+
+	int gameStarts = zoptions->ReadInt(zOPT_SEC_INTERNAL, "gameStarts", 0);
+	zoptions->WriteInt(zOPT_SEC_INTERNAL, "gameStarts", v8 + 1, 0);
+
+	zoptions->RemoveEntry(zOPT_SEC_INTERNAL, "testmode");
+	zoptions->RemoveEntry(zOPT_SEC_GAME, "weatherEffects");
+	zoptions->RemoveEntry(zOPT_SEC_INTERNAL, "zFastUnsafeSaveGames");
+
+	zoptions->Save(ini);
+
+	zSTRING game = zoptions->ParmValue("GAME");
+
+	if ( game.Length() > 0 ) {
+		zoptions->ChangeDir(DIR_SYSTEM);
+		zFILE* file = zfactory->CreateZFile(game);
+
+		if (!file || !file->Exists()) {
+			game += ".ini";
+			file = zfactory->CreateZFile(game);
+		}
+
+		if (file && file->Exists())
+			zgameoptions = new zCOption();
+			zgameoptions->Init("", 0);
+			zgameoptions->Load(game);
+			zSTRING forceparams = zgameoptions->ReadString(zOPT_SEC_OPTIONS, "force_Parameters", 0);
+			if (forceparams.Length() > 0)
+				AddParameters(forceparams);
+
+			file->Close();
+			delete file;
+		}
+	}
+
+	if ( zoptions->Parm("VDFS") ) {
+		zSTRING vdfs = zoptions->ParmValue("VDFS");
+		if (vdfs ==  "PHYSICALFIRST")
+			zFILE_VDFS::s_physical_first = 1;
+	}
+
+	bool recalc = zoptions->ReadBool(zOPT_SEC_PERFORMANCE, "recalc", 1);
+}
+
+void zDeinitOptions()
+{
+	if ( zoptions ) {
+		zoptions->WriteBool(zOPT_SEC_INTERNAL, "gameAbnormalExit", 0, 0);
+		zoptions->ChangeDir(0);
+
+		zSTRING ini = zoptions->ParmValue("ini");
+
+		if (ini.IsEmpty())
+			ini = "Gothic.ini";
+
+		zoptions->Save(ini);
+
+		Delete(zoptions);
+		Delete(zgameoptions);
+	}
+}
+
+void zCOption::AddParameters(zSTRING params)
+{
+	commandline = params + " " + commandline;
+	commandline.Upper();
+}
+
+void zCOption::ChangeDir(int dirId)
+{
+	auto dir = directory[dirId];
+	if ( dir ) {
+		dir->ChangeDir(0);
+	} else {
+		zFAULT("B: (zCOption::ChangeDir) directory-entry not defined!"); // 900
+	}
+}
+
+bool zCOption::EntryExists(zSTRING const& secName, const char* name)
+{
+	auto sec = GetSectionByName(secName, 0);
+	if ( sec )
+		return GetSectionByName(sec, name, 0) != 0;
+	return false;
+}
+
+bool zCOption::SectionExists(zSTRING const& secName)
+{
+  return GetSectionByName(secName, 1) != 0;
+}
+
+int zCOption::Parm(zSTRING const& parm)
+{
+	zSTRING search = "-" + parm;
+	search.Upper();
+	commandline.Upper();
+
+	return commandline.Search(0, search, 1u) >= 0;
+}
+
 int zCOption::ReadRaw(zSTRING const& secName, char const* optName, char *& outArray,
                       char* orArray, int orArraySize)
 {
@@ -104,7 +305,7 @@ int zCOption::ReadRaw(zSTRING const& secName, char const* optName, char *& outAr
 	}
 
 	zSTRING rawstr;
-	ReadString(this, rawstr, secName, optName, 0);
+	ReadString(rawstr, secName, optName, 0);
 
 	int len = rawstr.Length();
 

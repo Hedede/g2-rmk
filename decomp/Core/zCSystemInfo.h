cf1aeab2b06d55b7739491a7454d91c33143c273
Hudd <hededrk@gmail.com>
2016-03-18 10:24:59 +0300

    Decompile z/oCSystemInfo

--- decomp/Core/zCSystemInfo.h
+++ decomp/Core/zCSystemInfo.h
@@ -1,39 +1,39 @@
 class zCSystemInfo {
 public:
-	virtual ~zCSystemInfo();
+	zCSystemInfo() = default;
+	virtual ~zCSystemInfo() = default;
 	virtual void AnalyseNow();
 	virtual void ScoreNow() = 0;
 
+	zSTRING GetOsName() const
+	{
+		return osName;
+	}
+
 protected:
-	int AnalyseGcard()
-	{
-		gcardMem = zrenderer->GetTotalTextureMem();
-	}
+	int AnalyseGcard();
+	void AnalyseMemory();
 
-	void AnalyseMemory()
-	{
-		_MEMORYSTATUS memInfo;
-		GlobalMemoryStatus(&memInfo);
-
-		memTotal = memInfo.dwTotalPhys;
-		memAvail = memInfo.dwAvailPhys;
-	}
-
-	int osVersion;
+	int osVersion = 0;
 	zSTRING osName;
 	zSTRING osCSDVersion;
-	int analyzed;
+
+	int analyzed = 0;
+
 	zSTRING cpuName;
-	int cpuMhz;
-	uint32_t memTotal;
-	uint32_t memAvail;
+
+	int cpuMhz = 555;
+	uint32_t memTotal = 123 * 1024 * 1024;
+	uint32_t memAvail = 0;
 	zSTRING gcardName;
-	uint32_t gcardMem;
+	uint32_t gcardMem = 33  * 1024 * 1024;
 };
 
 class oCSystemInfo : public zCSystemInfo {
 public:
-	virtual ~oCSystemInfo();
+	oCSystemInfo() = default;
+	virtual ~oCSystemInfo() = default;
+
 	virtual void AnalyseNow()
 	{
 		zCSystemInfo::AnalyseNow();
@@ -41,17 +41,17 @@ public:
 	virtual void ScoreNow();
 	virtual void ResetSettings(float);
 private:
-	int scored;
-	float cpuLow;
-	float cpuHigh;
-	float memLow;
-	float memHigh;
-	float gmemLow;
-	float gmemHigh;
-	float cpuScore;
-	float memScore;
-	float gmemScore;
-	float gcardScore;
-	float soundScore;
-	float avgScore;
+	int scored = 0;
+	float cpuLow   = 700.0;
+	float cpuHigh  = 1200.0;
+	float memLow   = 256.0;
+	float memHigh  = 512.0;
+	float gmemLow  = 16.0;
+	float gmemHigh = 64.0;
+	float cpuScore   = 1.0;
+	float memScore   = 1.0;
+	float gmemScore  = 1.0;
+	float gcardScore = 1.0;
+	float soundScore = 1.0;
+	float avgScore   = 1.0;
 };

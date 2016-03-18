07f7a50d70d8e8e1d9997d75a88791ea6a167048
Hudd <hededrk@gmail.com>
2016-03-18 07:49:30 +0300

    Decode CSystemInfo fields

--- decomp/Core/oCSystemInfo.h
+++
@@ -1,7 +0,0 @@
-class oCSystemInfo : public zCSystemInfo {
-public:
-	virtual ~oCSystemInfo();
-	virtual void AnalyseNow();
-	virtual void ScoreNow();
-	virtual void ResetSettings(float);
-};
--- decomp/Core/zCSystemInfo.h
+++ decomp/Core/zCSystemInfo.h
@@ -1,4 +1,57 @@
 class zCSystemInfo {
+public:
 	virtual ~zCSystemInfo();
 	virtual void AnalyseNow();
+	virtual void ScoreNow() = 0;
+
+protected:
+	int AnalyseGcard()
+	{
+		gcardMem = zrenderer->GetTotalTextureMem();
+	}
+
+	void AnalyseMemory()
+	{
+		_MEMORYSTATUS memInfo;
+		GlobalMemoryStatus(&memInfo);
+
+		memTotal = memInfo.dwTotalPhys;
+		memAvail = memInfo.dwAvailPhys;
+	}
+
+	int osVersion;
+	zSTRING osName;
+	zSTRING osCSDVersion;
+	int analyzed;
+	zSTRING cpuName;
+	int cpuMhz;
+	uint32_t memTotal;
+	uint32_t memAvail;
+	zSTRING gcardName;
+	uint32_t gcardMem;
+};
+
+class oCSystemInfo : public zCSystemInfo {
+public:
+	virtual ~oCSystemInfo();
+	virtual void AnalyseNow()
+	{
+		zCSystemInfo::AnalyseNow();
+	}
+	virtual void ScoreNow();
+	virtual void ResetSettings(float);
+private:
+	int scored;
+	float cpuLow;
+	float cpuHigh;
+	float memLow;
+	float memHigh;
+	float gmemLow;
+	float gmemHigh;
+	float cpuScore;
+	float memScore;
+	float gmemScore;
+	float gcardScore;
+	float soundScore;
+	float avgScore;
 };

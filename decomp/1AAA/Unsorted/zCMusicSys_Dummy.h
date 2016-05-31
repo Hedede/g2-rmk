f4e41220dc3e735acb2649a63600703df8748cd2
Hudd <hededrk@gmail.com>
2016-06-01 02:30:06 +0300

    Add zCMusicSys_Dummy

--- decomp/1AAA/Unsorted/zCMusicSys_Dummy.h
+++
@@ -1,8 +0,0 @@
-class zCMusicSys_Dummy {
- virtual void ~zCMusicSys_Dummy(uint);
- virtual void LoadThemeByScript(zSTRING	const &);
- virtual void LoadTheme(zSTRING	const &);
- virtual void LoadJingleByScript(zSTRING const &);
- virtual void LoadJingle(zSTRING const &);
- virtual void DoMusicUpdate();
-};

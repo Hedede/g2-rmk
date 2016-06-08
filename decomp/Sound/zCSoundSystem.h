80e9d3c1030f228ac89442b4313e4ae8cc966c9f
Hudd <hededrk@gmail.com>
2016-06-08 23:21:22 +0300

    Update zCSoundSystem
    “Decompiled” bunch of stub methods.

--- decomp/Sound/zCSoundSystem.h
+++ decomp/Sound/zCSoundSystem.h
@@ -1,34 +1,109 @@
 class zCSoundSystem {
+	static zSTRING reverbPresetName[26];
+public:
+	static zSTRING const& GetReverbPresetName(int idx)
+	{
+		return reverbPresetName[idx];
+	}
+
+	static int GetNumReverbPresets()
+	{
+		return 26;
+	}
+
+	enum zTSpeakerType {};
+
+	struct zTSound3DParams {};
+
 public:
 	virtual ~zCSoundSystem();
-	virtual void LoadSoundFX(zSTRING const &);
-	virtual zCSoundFX* LoadSoundFXScript(zSTRING const &);
-	virtual void GetSFXParser();
-	virtual void GetPlayingTimeMSEC(zSTRING const &);
-	virtual void SetSound3DDefaultRadius(float);
-	virtual void GetSound3DDefaultRadius();
-	virtual void SetMasterVolume(float);
-	virtual void GetMasterVolume();
+	virtual zCSoundFX* LoadSoundFX(zSTRING const &)
+	{
+		return nullptr;
+	}
+	virtual zCSoundFX* LoadSoundFXScript(zSTRING const &)
+	{
+		return nullptr;
+	}
+	virtual zCParser* GetSFXParser()
+	{
+		return nullptr;
+	}
+	virtual float GetPlayingTimeMSEC(zSTRING const &)
+	{
+		return 0.0;
+	}
+
+	virtual void SetSound3DDefaultRadius(float radius)
+	{
+		snd3D_defaultRadius = radius;
+	}
+	virtual float GetSound3DDefaultRadius()
+	{
+		return snd3D_defaultRadius;
+	}
+
+	virtual void SetMasterVolume(float) {}
+	virtual float GetMasterVolume()
+	{
+		return 1.0;
+	}
+
 	virtual void PlaySound(zCSoundFX *,int,int,float,float) = 0;
 	virtual void PlaySound(zCSoundFX *,int) = 0;
-	virtual void PlaySound3D(zSTRING const &,zCVob *,int,zCSoundSystem::zTSound3DParams *) = 0;
-	virtual void PlaySound3D(zCSoundFX *,zCVob *,int,zCSoundSystem::zTSound3DParams *) = 0;
-	virtual void StopSound(int const &);
-	virtual void StopAllSounds();
-	virtual void GetSound3DProps(int const &,zCSoundSystem::zTSound3DParams &) = 0;
-	virtual void UpdateSound3D(int const &,zCSoundSystem::zTSound3DParams *) = 0;
-	virtual void GetSoundProps(int const &,int &,float &,float &);
-	virtual void UpdateSoundProps(int const &,int,float,float);
-	virtual void IsSoundActive(int const &);
-	virtual void SetListener(zCVob *);
-	virtual void SetGlobalReverbPreset(int,float);
-	virtual void SetReverbEnabled(int);
-	virtual void GetReverbEnabled();
-	virtual void GetNumProvider();
-	virtual void GetProviderName(int);
-	virtual void SetProvider(int);
-	virtual void SetSpeakerType(zCSoundSystem::zTSpeakerType);
-	virtual void GetSpeakerType();
+	virtual void PlaySound3D(zSTRING const &,zCVob *,int,zTSound3DParams *) = 0;
+	virtual void PlaySound3D(zCSoundFX *,zCVob *,int,zTSound3DParams *) = 0;
+	virtual void StopSound(int const &) {}
+	virtual void StopAllSounds() {}
+
+	virtual void GetSound3DProps(int const &,zTSound3DParams &) = 0;
+	virtual void UpdateSound3D(int const &,zTSound3DParams *) = 0;
+	virtual void GetSoundProps(int const &,int &,float &,float &) {}
+	virtual void UpdateSoundProps(int const &,int,float,float) {}
+	virtual bool IsSoundActive(int const &)
+	{
+		return false;
+	}
+	virtual void SetListener(zCVob *) {}
+	virtual void SetGlobalReverbPreset(int,float) { }
+	virtual void SetReverbEnabled(int) {}
+	virtual bool GetReverbEnabled()
+	{
+		return true;
+	}
+	virtual int GetNumProvider()
+	{
+		return 1;
+	}
+	virtual zSTRING* GetProviderName(int)
+	{
+		return nullptr;
+	}
+	virtual int SetProvider(int)
+	{
+		return 1;
+	}
+	virtual void SetSpeakerType(zCSoundSystem::zTSpeakerType) {}
+	virtual zTSpeakerType GetSpeakerType()
+	{
+		return 0;
+	}
 	virtual void SetGlobalOcclusion(float) = 0;
-	virtual void GetCPULoad();
+	virtual float GetCPULoad()
+	{
+		return -1.0;
+	}
+
+private:
+	float snd3D_defaultRadius;
+};
+
+zSTRING zCSoundSystem::reverbPresetName[26] = {
+	"GENERIC", "PADDEDCELL", "ROOM", "BATHROOM",
+	"LIVINGROOM", "STONEROOM", "AUDITORIUM", "CONCERTHALL",
+	"CAVE", "ARENA", "HANGAR", "CARPETEDHALLWAY",
+	"HALLWAY", "STONECORRIDOR", "ALLEY", "FOREST",
+	"CITY", "MOUNTAINS", "QUARRY", "PLAIN",
+	"PARKINGLOT", "SEWERPIPE", "UNDERWATER", "DRUGGED",
+	"DIZZY", "PSYCHOTIC"
 };
--- decomp/Sound/zCSoundSystemDummy.h
+++ decomp/Sound/zCSoundSystemDummy.h
@@ -1,12 +1,33 @@
-class zCSoundSystemDummy {
- virtual void ~zCSoundSystemDummy(uint);
- virtual void LoadSoundFX(zSTRING const &);
- virtual void PlaySound(zCSoundFX *,int,int,float,float);
- virtual void PlaySound(zCSoundFX *,int);
- virtual void PlaySound3D(zSTRING const &,zCVob *,int,zCSoundSystem::zTSound3DParams	*);
- virtual void PlaySound3D(zCSoundFX *,zCVob *,int,zCSoundSystem::zTSound3DParams *);
- virtual void GetSound3DProps(int const &,zCSoundSystem::zTSound3DParams &);
- virtual void UpdateSound3D(int const &,zCSoundSystem::zTSound3DParams	*);
- virtual void DoSoundUpdate();
- virtual void SetGlobalOcclusion(float);
+class zCSoundSystemDummy : zCSoundSystem {
+	virtual ~zCSoundSystemDummy();
+	zCSoundFX* LoadSoundFX(zSTRING const &) override
+	{
+		return new zCSoundFXDummy;
+	}
+	void PlaySound(zCSoundFX *,int,int,float,float) override
+	{
+		return nullptr;
+	}
+	void PlaySound(zCSoundFX *,int) override
+	{
+		return nullptr;
+	}
+	void PlaySound3D(zSTRING const &,zCVob *,int,zCSoundSystem::zTSound3DParams	*) override
+	{
+		return nullptr;
+	}
+	void PlaySound3D(zCSoundFX *,zCVob *,int,zCSoundSystem::zTSound3DParams *) override
+	{
+		return nullptr;
+	}
+	void GetSound3DProps(int const &,zCSoundSystem::zTSound3DParams &) override
+	{
+		return nullptr;
+	}
+	void UpdateSound3D(int const &,zCSoundSystem::zTSound3DParams*) override
+	{
+		return nullptr;
+	}
+	void DoSoundUpdate() override {}
+	void SetGlobalOcclusion(float) override {}
 };

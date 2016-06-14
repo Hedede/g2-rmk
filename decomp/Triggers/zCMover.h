223b92f3abf25cbd22446252ea8329b98a2a14e4
Hudd <hededrk@gmail.com>
2016-06-14 12:06:59 +0300

    Decompile more of zCMover

--- decomp/Triggers/zCMover.h
+++ decomp/Triggers/zCMover.h
@@ -6,12 +6,32 @@ class zTMov_Keyframe {
 class zCMover : public zCTrigger {
 	Z_OBEJCT(zCMover);
 public:
-	virtual ~zCMover();
+	zCMover()
+		: zCTrigger()
+	{
+		SetCollisionClass(&zCCollObjectComplex::s_oCollObjClass);
+		SetCollDetStat(0);
+
+		filterFlags &= 0b11111001; // TODO: prettify
+		filterFlags |= 1;
+
+		flags1 &= 0b11001111;
+		flags1 ^= 0b10000;
+
+		ClearStateInternals();
+	}
+
+	virtual ~zCMover()
+	{
+		Release(sfxMoving);
+		Release(soundOpenEnd);
+	}
+
 	void Archive(zCArchiver& arc) override;
 	void Unarchive(zCArchiver& arc) override;
 
-	void OnTrigger(zCVob *,zCVob *) override;
-	void OnUntrigger(zCVob *,zCVob *) override;
+	void OnTrigger(zCVob* other, zCVob* instigator) override;
+	void OnUntrigger(zCVob* other, zCVob* instigator) override;
 	void OnTouch(zCVob *) override;
 	void OnDamage(zCVob*, zCVob* vobAttacker, float damage, int, zVEC3 const&) override;
 	{
@@ -24,8 +44,16 @@ public:
 
 	void PostLoad() override;
 
-	void CanThisCollideWith(zCVob *) override;
+	void CanThisCollideWith(zCVob* other) override;
 	void SetVisual(zCVisual	*) override;
+
+	bool IsKeyToThisMover(zCVob* other)
+	{
+		if (respondToVobName.IsEmpty())
+			return 1;
+
+		return other->GetObjectName() == respondToVobName;
+	}
 
 	void AdvanceKeyframe_KF()
 	{
@@ -42,12 +70,18 @@ public:
 		ShowDebugInfo(this);
 	}
 
-private:
-	struct zTMov_Keyframe {
-		zPOINT3     pos;
-		zCQuat      quat;
-	};
+	void DoClose();
+	void DoOpen();
+	void InvertMovement();
 
+private:
+	void StartMovingSound();
+	void MoveToKeyFrame(int idx);
+	void FinishedOpening();
+	void ClearStateInternals();
+	void UpdateInternals();
+
+private:
 	zCArray<zTMov_Keyframe>   keyframeList;
 
 	// internals
@@ -72,14 +106,14 @@ private:
 
 	zCModel*       model;
 	zTSoundHandle  soundMovingHandle;
-	zCSoundFX*     sfxMoving;
+	zCSoundFX*     sfxMoving = nullptr;
 
-	zREAL  moveSpeed;
-	zREAL  stayOpenTimeSec;
-	zREAL  touchBlockerDamage;
+	zREAL  moveSpeed          = 0.3;
+	zREAL  stayOpenTimeSec    = 2.0;
+	zREAL  touchBlockerDamage = 0.0;
 
-	bool moverLocked;
-	bool autoLinkEnabled;
+	bool moverLocked     = false;
+	bool autoLinkEnabled = false;
 	bool autoRotate;
 	// padding 1b
 
@@ -97,20 +131,20 @@ private:
 		MB_NSTATE_LOOP,
 		MB_NSTATE_SINGLE_KEYS
 	};
-	zTMoverBehavior moverBehavior;
+	zTMoverBehavior moverBehavior = 0;
 
 	enum zTTouchBehavior {
 		TB_TOGGLE,
 		TB_WAIT
 	};
-	zTTouchBehavior touchBehavior;
+	zTTouchBehavior touchBehavior = 0;
 
 	// aniType: Keyframe
 	enum zTPosLerpType {
 		PL_LINEAR,
 		PL_CURVE
 	};
-	zTPosLerpType posLerpType;
+	zTPosLerpType posLerpType = 1;
 
 	enum zTSpeedType {
 		ST_CONST,
@@ -121,7 +155,7 @@ private:
 		ST_SEG_SLOW_START,
 		ST_SEG_SLOW_END,
 	};
-	zTSpeedType speedType;
+	zTSpeedType speedType = 1;
 
 	// sound
 	zSTRING  soundOpenStart;
@@ -262,4 +296,170 @@ void zCMover::Unarchive(zCArchiver& arc)
 
 	if ( !arc.InSaveGame() )
 		SetToKeyframe(0.0, 0.0);
+}
+
+void zCMover::MoveToKeyframe(int idx)
+{
+	if ( advanceDir == 0.0 && moverBehavior == 4 ) {
+		SetSleeping(0);
+
+		moverState = 1;
+
+		CLAMP(idx, 0, keyframeList.GetNumInList());
+
+		nextKeyframe = idx;
+
+		SetToKeyframe(actKeyframeF, 1.0);
+	}
+}
+
+void zCMover::FinishedOpening()
+{
+	SetSleeping(1);
+
+	moverState = 0;
+
+	zsound->StopSound(&soundMovingHandle);
+	zsound->PlaySound3D(&soundOpenEnd, this, 1, 0);
+
+	if ( moverBehavior == 2 ) {
+		stayOpenTimeDest = stayOpenTimeSec * 1000.0 + ztimer.totalTimeFloat;
+		SetSleeping(0);
+	}
+
+	Init();
+}
+
+void zCMover::UpdateInternals()
+{
+	Release(sfxMoving);
+
+	if ( soundMoving.Length() > 0 )
+		sfxMoving = zsound->LoadSoundFX(soundMoving);
+
+	model = dynamic_cast<zCModel*>(visual);
+}
+
+int zCMover::CanThisCollideWith(zCVob* other)
+{
+	if (auto trig = dynamic_cast<zCTriggerBase*>(other))
+		return 0;
+	if (other->flags1.staticVob)
+		return 0;
+	if (!other->HasParentVob())
+		return 0;
+
+	return !autoLinkEnabled || other->CanAutoLinkWith(this);
+}
+
+void zCMover::StartMovingSound()
+{
+	zTSound3DParams params;
+
+	params.unk0 = 0;
+	params.unk1 = 1.0;
+	params.unk2 = -1.0;
+	params.unk4 = 0;
+	params.unk5 = 1.0;
+	params.unk6 = 0;
+	params.unk7 = -999999.0;
+	params.unk3 = 1;
+	soundMovingHandle = zsound->PlaySound3D(sfxMoving, this, 2, &params);
+}
+
+void zCMover::DoOpen()
+{
+	zCVob::SetSleeping(0);
+
+	moverState = 1;
+
+	SetToKeyframe(0, 1.0);
+
+	zsound->PlaySound3D(soundOpenStart, this, 1, 0);
+
+	StartMovingSound();
+}
+
+void zCMover::DoClose()
+{
+	SetSleeping(0);
+
+	moverState = 3;
+
+	SetToKeyframe(keyframeList.Size(), -1.0);
+
+	zsound->PlaySound3D(soundCloseStart, this, 1, 0);
+
+	StartMovingSound();
+}
+
+void zCMover::InvertMovement()
+{
+	if ( moverBehavior <= 2 ) {
+		if ( moverState == 1 ) {
+			moverState = 3;
+		} else if ( moverState == 3 ) {
+			moverState = 1;
+		}
+	}
+
+	SetToKeyframe(actKeyframeF, -advanceDir);
+
+	if ( moverAniType == 1 ) {
+		for (unsigned i = 0; i < model->active_ani_num; ++i) {
+			auto active = model->active_anis[i];
+			active->SetDirection(active->someflag != -1);
+		}
+	}
+}
+
+void zCMover::OnUntrigger(zCVob* other, zCVob* instigator)
+{
+	if (trg_flags & 2) {
+		zINFO(8,"D: zMOV: Untrigger Mover " + GetObjectName()); // _dieter/zVobMisc.cpp, 1285
+		if ( moverBehavior == 1 && --numTriggerEvents <= 0 ) {
+			if ( moverState == 0 ) {
+				DoClose();
+			} else if ( moverState == 1) {
+				InvertMovement();
+			}
+		}
+	}
+}
+
+void zCMover::OnTouch(zCVob *other)
+{
+	if ( autoLinkEnabled )
+		other->CheckAutoLink(this);
+
+	if ( trg_flags & 2 && other ) {
+		if ( flags2.sleepingMode && advanceDir != 0.0 ) {
+			if ( touchBlockerDamage > 0.0 ) {
+				zVEC3 pos;
+				other->GetEM()->OnDamage(this, this, touchBlockerDamage, 1, pos);
+			}
+		} else if ( trg_filterFlags & 2 ) {
+			TriggerMover(other);
+		}
+	}
+}
+
+void zCMover::ClearStateInternals()
+{
+	actKeyPosDelta = zVEC3{0,0,0};
+	actKeyframeF = 0;
+	actKeyframe = 0;
+	nextKeyframe = 0;
+	moveSpeedUnit = 0;
+	advanceDir = 0;
+	moverState = 2;
+	numTriggerEvents = 0;
+	stayOpenTimeDest = 0;
+	model = 0;
+	autoRotate = 0;
+
+	if ( sfxMoving ) {
+		zsound->StopSound(soundMovingHandle);
+		Release(sfxMoving);
+	}
 }

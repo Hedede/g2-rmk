33114d616d6403b317c1fc02ff59de0b76cdd523
Hudd <hededrk@gmail.com>
2016-06-16 16:44:23 +0300

    Decompile o/zCWaypoint

--- decomp/WayNet/oCWaypoint.h
+++ decomp/WayNet/oCWaypoint.h
@@ -1,7 +1,16 @@
 class oCWaypoint : public zCWaypoint {
 	Z_OBJECT(oCWaypoint);
 public:
-	virtual ~oCWaypoint();
+	oCWaypoint() = default;
+	~oCWaypoint() override = default;
 
-	virtual void CanBeUsed(zCVob const *);
+	void CanBeUsed(zCVob const* vob) override
+	{
+		if (auto npc = dynamic_cast<oCNpc const*>(vob)) {
+			return waterDepth <= 0.0 ||
+			       npc->CanSwim()    ||
+			       npc->CanDive() && underWater;
+		}
+		return true;
+	}
 };
--- decomp/WayNet/zWaypoint.h
+++ decomp/WayNet/zWaypoint.h
@@ -1,7 +1,8 @@
 class zCWaypoint : public zCObject {
 	Z_OBJECT(zCWaypoint);
 public:
-	virtual ~zCWaypoint();
+	zCWaypoint() = default;
+	virtual ~zCWaypoint() = default;
 
 	void Archive(zCArchiver& arc) override
 	{
@@ -31,11 +32,23 @@ public:
 		return wayList.GetNumInList();
 	}
 
+	void SetName(zSTRING& name)
+	{
+		this->name = name;
+		if (wpvob)
+			wpvob->SetVobName(name);
+	}
+
 	zSTRING GetName()
 	{
 		if (wpvob)
 			name = wpvob->GetObjectName();
 		return name;
+	}
+
+	void Init(float x, float y, float z)
+	{
+		Init(zVEC3{x,y,z});
 	}
 
 	void Init(zVEC3& pos)
@@ -101,29 +114,99 @@ public:
 		return false;
 	}
 
+	void Free()
+	{
+		for (auto way : wayList)
+			if (way)
+				delete way;
+
+		parent = nullptr;
+		if (wpvob && wpvob->homeWorld)
+			wpvob->homeWorld->RemoveVob(wpvob);
+		Release(wpvob);
+	}
+
+	void UpdatePositionWorld()
+	{
+		if (wpvob) {
+			pos = wpvob->GetPositionWorld();
+			dir = wpvob->GetHeadingAtWorld();
+		}
+	}
+
+	void CorrectHeight(zCWorld* wld)
+	{
+		zVEC3 ray{0, -1000.0, 0};
+		if ( wld->TraceRayNearestHit(pos, ray, wpvob, 0x821) && (wld->foundPoly || wld->foundVob) ) {
+			pos = wld->foundIntersection;
+			pos.y += 50.0;
+
+			if ( wpvob )
+				wpvob->SetPositionWorld(pos);
+		}
+	}
+
+	void CalcProperties(zCWorld* wld);
+
 private:
 	// Kürzeste Weg Suche durch das zCWaynet
 	// Diese Eigenschaften besser nicht nutzen,
 	// es sind A* (Zwischen-) Ergebnisse.
-	int routeCtr;
-	int curCost;
-	int estCost;
-	int score;
-	int curList;
-	zCWay* parent;
+	int routeCtr = 0;
+	int curCost  = 0;
+	int estCost  = 0;
+	int score    = 0;
+	int curList  = 0;
+	zCWay* parent = nullptr;
 
 	//Sonstige Daten
-	int waterDepth;
-	zBOOL underWater;
+	int waterDepth = 0;
+	zBOOL underWater = false;
 
-	zVEC3 pos;         // Position dieses Waypoints
-	zVEC3 dir;         // AtVector
+	// Position dieses Waypoints
+	zVEC3 pos{0,0,0};
+	// AtVector
+	zVEC3 dir{1.0,0,0};
 	zSTRING name;
 
 	// dazugehöriger Vob in dér Welt
-	zCVobWaypoint* wpvob;
+	zCVobWaypoint* wpvob = nullptr;
 
 	// Liste der hier beginnenden Wege ( Ways )
 	zCList<zCWay> wayList;
 };
 
+void zCWaypoint::CalcProperties(zCWorld *wld)
+{
+	waterDepth = 0;
+	float dist = 0.0;
+
+	zVEC3 start = pos;
+	zVEC3 ray{0, -1000.0, 0};
+	if ( wld->TraceRayNearestHit(start, ray, wpvob, 0xA21) ) {
+		auto foundPoly = wld->foundPoly;
+		if (foundPoly && foundPoly->material) {
+			dist = pos.y - wld->foundIntersection.y;
+			if ( foundPoly->material->matGroup == MAT_WATER ) {
+				start = wld->foundIntersection;
+				if ( wld->TraceRayNearestHit(start, ray, wpvob, 0x821) ) {
+					if ( wld->foundPoly ) {
+						waterDepth += start.y - wld->foundIntersection.y;
+					} else {
+						zWARNING("U: WAY: Incorrect Waterdepth at WP " + GetName()); // 948, _ulf\\zWaynet.cpp
+					}
+				}
+			}
+		}
+	}
+
+	ray = zVEC3{0, 1000, 0};
+	if ( wld->TraceRayNearestHit(start, ray, wpvob, 0xA21) ) {
+		if ( wld->foundPoly && wld->foundPoly->material ) {
+			if ( wld->foundPoly->material->matGroup == MAT_WATER ) {
+				waterDepth = wld->foundIntersection.y - pos.y + dist;
+				underWater = 1;
+			}
+		}
+	}
+}

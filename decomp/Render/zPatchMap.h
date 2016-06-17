cab22d80c29ccf948525c569872ebb4784efa5bf
Hudd <hededrk@gmail.com>
2016-06-17 21:12:52 +0300

    Add zCPatchMap (fields only)

---
+++ decomp/Render/zPatchMap.h
@@ -0,0 +1,49 @@
+/*
+ * As far as I see,
+ * PatchMap used to calculate lightning.
+ */
+struct zCPatch {
+	zVEC3 vec0;
+	zVEC3 vec1;
+	zVEC3 vec2;
+	float __grid2;
+	zVEC3 vec3;
+	zVEC3 vec4;
+	__int16 unk3;
+	__int16 unk4;
+	zCArray arr0;
+	zVEC3 vec6;
+};
+
+#pragma pack(push, 1)
+struct zCPatchMap {
+	~zCPatchMap() {
+		for (auto patch : patches) {
+			if (patch) {
+				patch->arr0.DeleteList();
+				delete patch;
+			}
+		}
+
+		unk = 0;
+	}
+
+	char unk0;
+	__int16 unk1;
+	__int16 unk2;
+
+	zCArray<zCPatch*>   patches;
+	zCArray<zCPolygon*> surface;
+
+	zTPlane plane0;
+	zTPlane plane1;
+
+	zTBBox3D bbox;
+
+	zVEC3 __normal_maybe;
+	zVEC3 vec0;
+	zVEC3 vec1;
+
+	zCPolygon* poly0;
+};
+#pragma pack(pop)

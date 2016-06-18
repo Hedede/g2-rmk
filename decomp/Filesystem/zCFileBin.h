1ab19e802bfe9ad5f21be709233c814666f1a411
Hudd <hededrk@gmail.com>
2016-06-18 03:37:03 +0300

    Decompile most of zCFileBin

--- decomp/Filesystem/zCFileBin.h
+++ decomp/Filesystem/zCFileBin.h
@@ -1,3 +1,43 @@
+struct zCFileBIN {
+	zCFileBIN() = default;
+	~zCFileBIN()
+	{
+		if (file)
+			BinClose();
+	}
+
+	void BinCreate(zSTRING const& fileName);
+	void BinSetFile(zFILE* file);
+	void BinClose();
+	void BinWriteInt(int i);
+	void BinWriteFloat(float f);
+	void BinWriteString(zSTRING const& s);
+	void BinStartChunk(unsigned short id);
+	void BinEndChunk();
+	void BinSkipChunk();
+
+private:
+	int chunk_start = -1;
+	int unk1  = -1;
+	int is_open = 0;
+	zFILE *file = nullptr;
+};
+
+void zCFileBIN::BinCreate(zSTRING const& fileName)
+{
+	file = zfactory->CreateZFile(fileName);
+	file->Create();
+	chunk_start = -1;
+}
+
+void zCFileBIN::BinSetFile(zFILE *file)
+{
+	file = file;
+	unk = -1;
+	chunk_start = -1;
+	is_open = 1;
+} 
+
 void zCFileBIN::BinWriteInt(int i)
 {
 	file->Write(&i, sizeof(int));
@@ -13,10 +53,44 @@ void zCFileBIN::BinWriteString(zSTRING const& str)
 	file->Write(str + "\n");
 }
 
-void zCFileBIN::BinSetFile(zFILE *file)
+void zCFileBIN::BinEndChunk()
 {
-	this->file = file;
-	this->unk[1] = -1;
-	this->unk[0] = -1;
-	this->unk[2] = 1;
-} 
+	if ( chunk_start != -1 ) {
+		auto pos = file->Pos();
+		auto sz = pos - chunk_start - 6;
+		file->Seek(chunk_start + 2);
+		file->Write(&sz, 4);
+		file->Seek(pos);
+	}
+}
+
+void zCFileBIN::BinStartChunk(unsigned short id)
+{
+	if ( file ) {
+		BinEndChunk();
+
+		unsigned dummy = 0;
+		chunk_start = file->Pos();
+
+		file->Write(&id, 2);
+		file->Write(&dummy, 4); // size
+	}
+}
+
+void zCFileBIN::BinClose()
+{
+	BinEndChunk();
+	if ( !is_open ) {
+		file->Close();
+		Delete(file);
+	}
+	file = nullptr;
+}
+
+void zCFileBIN::BinSkipChunk()
+{
+	if ( file ) {
+		if ( unk1 < file->Size() )
+			file->Seek(unk1);
+	}
+}

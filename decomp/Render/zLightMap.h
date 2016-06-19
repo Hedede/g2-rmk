520735be21e8637f9b75463ee47c7eb871a562c0
Hudd <hededrk@gmail.com>
2016-06-19 16:24:50 +0300

    Merge zPatchMap.h and zLightMap.h

--- decomp/Render/zLightMap.h
+++ decomp/Render/zLightMap.h
@@ -26,3 +26,56 @@ private:
 	zVEC3 lmRight;
 	zCTexture* texture = nullptr;
 };
+
+/*
+ * As far as I see,
+ * PatchMap used to calculate lightning.
+ */
+struct zCPatch {
+	zCPatch() = default;
+
+	zVEC3 vec0;
+	zVEC3 vec1;
+	zVEC3 vec2;
+	float __grid2;
+	zVEC3 vec3;
+	zVEC3 vec4;
+	__int16 unk3;
+	__int16 unk4;
+	zCArray arr0;
+	zVEC3 vec6;
+};
+
+#pragma pack(push, 1)
+struct zCPatchMap {
+	~zCPatchMap() {
+		for (auto patch : patches) {
+			if (patch) {
+				patch->arr0.DeleteList();
+				delete patch;
+			}
+		}
+
+		poly0 = 0;
+		// also implicit deletings of arrays
+	}
+
+	char unk0;
+	__int16 unk1;
+	__int16 unk2;
+
+	zCArray<zCPatch*>   patches;
+	zCArray<zCPolygon*> surface;
+
+	zTPlane plane0;
+	zTPlane plane1;
+
+	zTBBox3D bbox;
+
+	zVEC3 __normal;
+	zVEC3 __up;
+	zVEC3 __right;
+
+	zCPolygon* poly0;
+};
+#pragma pack(pop)

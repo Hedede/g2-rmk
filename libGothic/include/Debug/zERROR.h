8765c4bccfe45657d80c37f39622d78e58afbe33
Hudd <hededrk@gmail.com>
2016-08-21 17:15:05 +0300

    Add zERROR.h and zExceptionHandler.h

---
+++ libGothic/include/Debug/zERROR.h
@@ -0,0 +1,15 @@
+#ifndef GOTHIC_ZERROR_H
+#define GOTHIC_ZERROR_H
+#include <Types/zSTRING.h>
+
+struct zERROR {
+	void Init(std::string const& cmdLine)
+	{
+		Thiscall<void(zERROR*, zSTRING)> func{0x44BFA0};
+		func(this, cmdLine);
+	}
+};
+
+zERROR& zerr = *static_cast<zERROR*>(0x8CDCD0);
+
+#endif//GOTHIC_ZERROR_H
---
+++ libGothic/include/Debug/zExceptionHandler.h
@@ -0,0 +1,18 @@
+#ifndef GOTHIC_ZCEXCEPTIONHANDLER
+#define GOTHIC_ZCEXCEPTIONHANDLER
+#include <Types/Base.h>
+#include <Hook/func.h>
+
+zBOOL& zExHandler = *reinterpret_cast<zBOOL*>(0x89D9F4);
+
+struct zCExceptionHandler {
+	SetIsActive(bool active)
+	{
+		Cdecl<void(int)> func{0x4C887C};
+
+		zExHandler = active;
+		func(active);
+	}
+};
+
+#endif//GOTHIC_ZCEXCEPTIONHANDLER

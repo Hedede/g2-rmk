e3ad3f23390121cab4cb6c361c590482df0b4289
Hudd <hededrk@gmail.com>
2016-08-21 17:13:46 +0300

    Clean up new main
    — Gets called instead of WinMain now (was called by HandledWinMain
    before)

--- libGothic/include/Types/zSTRING.h
+++ libGothic/include/Types/zSTRING.h
@@ -1,11 +1,22 @@
 #ifndef G2_zSTRING_h
 #define G2_zSTRING_h
+#include <string>
 #include <Types/Base.h>
 
 char const spaceChar = ' ';
 
 struct zSTRING {
 	zSTRING() = default;
+
+	zSTRING(char const* str)
+	{
+		Thiscall<void(zSTRING*, char const*)> ctor{0x4010C0};
+		ctor(this, str);
+	}
+
+	zSTRING(std::string const& str)
+		: zSTRING(str.c_str())
+	{ }
 
 private:
 	void* _vtab;
---
+++ libRemake/src/Main.cpp
@@ -0,0 +1,99 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <string>
+
+auto libExit = func<void(*)()>(0x502AB0);
+auto sysEvent = func<void(*)()>(0x5053E0);
+
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+void put(std::string s)
+{
+	fwrite(s.c_str(), s.size(), 1, stdout);
+}
+
+void put(char const* w)
+{
+	put(std::string(w));
+}
+
+template<typename T>
+void put(T t)
+{
+	put(std::to_string(t));
+}
+
+template<typename... Args>
+void print(Args... args)
+{
+	( put(args), ... );
+}
+
+template<typename... Args>
+void println(Args... args)
+{
+	( put(args), ..., put("\n"));
+}
+
+#include <Debug/zERROR.h>
+
+void*& hInstApp = *reinterpret_cast<void**>(0x8D4220);
+void*& sysInstLock = *reinterpret_cast<void**>(0x8D426C);
+char(&sysLogName)[260] = *reinterpret_cast<char*>(0x8D3A90);
+
+unsigned& winMainThreadID = *reinterpret_cast<unsigned*>(0x8D3D34);
+void*& winMainThread = *reinterpret_cast<void**>(0x8D3ED0);
+
+#include <new>
+void* sysAlloc(size_t bytes)
+u
+	return ::operator new(bytes);
+}
+
+#include <aw/utility/string.h>
+extern "C" void aw_main(void* hinst, char const* args)
+{
+	freopen("log.txt", "wb", stdout);
+
+	print("-- LIBRARY LOADED --\n");
+
+	InitCommonControls();
+
+	hInstApp = hinst;
+
+	std::string cmdLine{args};
+	aw::string::toupper(cmdLine);
+	zerr.Init(cmdLine);
+
+	bool handler = cmdLine.find("ZNOEXHND") == std::string::npos;
+	zCExceptionHandler::SetIsActive(handler);
+
+	// TODO: remove k chertyam sobachim
+	std::string log = "DemoW.log";
+	std::copy(begin(log), end(log), std::begin(sysLogName));
+
+	winMainThreadID = GetCurrentThreadId();
+	void* a = GetCurrentProcess();
+	void* b = GetCurrentThread();
+	void* c = GetCurrentProcess();
+	DuplicateHandle(a, b, c, &winMainThread, 0, 0, 2u);
+
+
+	exit(0);
+}
+
+void GameLoop()
+{
+	CGameManager game;
+
+	sysEvent();
+
+	game.Init((void*)0x8D422C);
+	game.Run();
+	game.Done();
+
+	libExit();
+}

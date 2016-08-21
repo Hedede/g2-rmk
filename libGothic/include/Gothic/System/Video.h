290a18a1f2cea8788dbb614b3a88badbd84c76c1
Hudd <hededrk@gmail.com>
2016-08-21 22:59:11 +0300

    Add System/Video.h
    (forgot to include in previous commits)

---
+++ libGothic/include/Gothic/System/Video.h
@@ -0,0 +1,11 @@
+#ifndef GOTHIC2_VIDEO_H
+#define GOTHIC2_VIDEO_H
+
+#include <Hook/func.h>
+#include <Hook/value.h>
+
+namespace {
+auto& vidHideMenu = Value<int>(0x8D38D8);
+}
+
+#endif//GOTHIC2_VIDEO_H

336c352a88c211f5ee0e2716e18a061c2ae4e611
Hudd <hededrk@gmail.com>
2016-08-23 19:05:41 +0300

    decomp: Add some code

---
+++ decomp/code/Menu/oCViewDialogItemContainer.h
@@ -0,0 +1,3 @@
+struct oCViewDialogItemContainer : zCView {
+	void UpdateValue() { }
+};
--- decomp/code/Video/zCBinkPlayer.h
+++ decomp/code/Video/zCBinkPlayer.h
@@ -5,11 +5,32 @@ struct zCBinkPlayer : zCVideoPlayer {
 	virtual void PlayInit(int);
 	virtual void PlayFrame();
 	virtual void PlayDeinit();
-	virtual void Pause();
+	virtual void Pause()
+	{
+		BinkPause(binkHandle, 1);
+		return zCVideoPlayer::Pause();
+	}
 	virtual void Unpause();
+	{
+		BinkPause(binkHandle, 0);
+		return zCVideoPlayer::Unpause();
+	}
 	virtual void IsPlaying();
-	virtual void ToggleSound();
-	virtual void SetSoundVolume(float);
+	virtual void ToggleSound()
+	{
+		zCVideoPlayer::ToggleSound();
+		if ( soundEnabled ) {
+			SetSoundVolume(1.0);
+		} else {
+			SetSoundVolume(0.0);
+		}
+		return soundEnabled;
+	}
+	virtual void SetSoundVolume(float vol)
+	{
+		BinkSetVolume(binkHandle, 0, (vol * 65536.0));
+	}
+
 	virtual void PlayGotoNextFrame();
 	virtual void PlayWaitNextFrame()
 	{

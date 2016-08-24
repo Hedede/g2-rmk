4de861ddaa12950469be705b3871222cd33cd63d
Hudd <hededrk@gmail.com>
2016-08-24 18:59:20 +0300

    decomp: Decompile z/oCObjectFactory

--- decomp/code/zCObject/oObjectFactory.h
+++ decomp/code/zCObject/oObjectFactory.h
@@ -1,14 +1,28 @@
 class oCObjectFactory : public zCObjectFactory {
 	Z_OBJECT(oCObjectFactory);
 public:
-	virtual ~oCObjectFactory();
-	virtual void CreateEventManager(zCVob *);
-	virtual void CreateCSManager();
-	virtual void CreateGameInfo();
-	virtual void CreatePlayerInfo();
-	virtual void CreateWorld();
-	virtual void CreateWaypoint();
-	virtual void CreateWay();
-	virtual void CreateNpc(int);
-	virtual void CreateItem(int);
+	virtual ~oCObjectFactory() = default;
+	zCEventManager* CreateEventManager(zCVob *) override { return new zCEventManager; }
+	oCCSManager* CreateCSManager() override { return new oCCSManager; }
+	oCGameInfo* CreateGameInfo() override { return new oCGameInfo; }
+	oCPlayerInfo* CreatePlayerInfo() override { return new oCPlayerInfo; }
+	oCWorld* CreateWorld() override { return new oCWorld; }
+	oCWaypoint* CreateWaypoint() override { return new oCWaypoint; }
+	oCWay* CreateWay() override { return new oCWay; }
+	virtual oCNpc* CreateNpc(int inst)
+	{
+		if (inst == -1)
+			return new oCNpc;
+		auto npc = new oCNpc;
+		npc->InitByScript(inst, 1);
+		return npc;
+	}
+	virtual oCItem* CreateItem(int inst)
+	{
+		if (inst == -1)
+			return new oCItem;
+		auto it = new oCItem;
+		it->InitByScript(inst, 1);
+		return it;
+	}
 };
--- decomp/code/zCObject/zObjectFactory.h
+++ decomp/code/zCObject/zObjectFactory.h
@@ -1,15 +1,15 @@
 class zCObjectFactory : public zCObject {
 	Z_OBJECT(zCObjectFactory);
 public:
-	virtual ~zCObjectFactory();
-	virtual void CreateEventManager(zCVob *);
-	virtual void CreateZFile(zSTRING const &);
-	virtual void CreateSession();
-	virtual void CreateCSManager();
-	virtual void CreateNetVobControl(zCVob *);
-	virtual void CreateGameInfo();
-	virtual void CreatePlayerInfo();
-	virtual void CreateWorld();
-	virtual void CreateWaypoint();
-	virtual void CreateWay();
+	virtual ~zCObjectFactory() = default;
+	virtual zCEventManager* CreateEventManager(zCVob *vob) { return new zCEventManager(vob); }
+	virtual zFILE* CreateZFile(zSTRING const& fn) { return zFILE_VDFS(fn); }
+	virtual zCSession* CreateSession() { return new zCSession; }
+	virtual zCCSManager* CreateCSManager() { return new zCCSManager; }
+	virtual zCNetVobControl* CreateNetVobControl(zCVob *vob) { return new zCNetVobControl(vob); }
+	virtual zCGameInfo* CreateGameInfo() { return 0; }
+	virtual zCPlayerInfo* CreatePlayerInfo() { return 0; }
+	virtual zCWorld* CreateWorld() { return new zCWorld; }
+	virtual zCWayPoint* CreateWaypoint() { return zCWayPoint; }
+	virtual zCWay* CreateWay() { return new zCWay; }
 };

cceb18ea394ea23af8bf3a98dd3eb1ced1ac1329
Hudd <hededrk@gmail.com>
2016-08-24 05:14:47 +0300

    decomp: Decopile zCBinkPlayer

--- decomp/code/Video/oCBinkPlayer.h
+++ decomp/code/Video/oCBinkPlayer.h
@@ -1,12 +1,13 @@
+// _bert\\oBinkPlayer.cpp
 struct oCBinkPlayer : zCBinkPlayer {
 	oCBinkPlayer() : zCBinkPlayer()
 	{
 		extendedVideoKeys = false;
 	}
 	virtual ~oCBinkPlayer() = default;
-	virtual void OpenVideo(zSTRING);
+	int OpenVideo(zSTRING fileName) override;
 
-	virtual int PlayInit(int frameNr)
+	int PlayInit(int frameNr) override
 	{
 		disallowVideoInput = zoptions->ReadBool(zOPT_SEC_GAME, "disallowVideoInput", 0);
 		int ret = zCBinkPlayer::PlayInit(frameNr);
@@ -24,9 +25,20 @@ struct oCBinkPlayer : zCBinkPlayer {
 	{
 		zCBinkPlayer::PlayDeinit();
 	}
-	virtual void PlayHandleEvents();
+	void PlayHandleEvents() override;
 
 private:
 	zBOOL extendedVideoKeys;
 	zBOOL disallowVideoInput;
 };
+
+int OpenVideo(zSTRING fileName)
+{
+	zINFO_B(3,"B: VP: Start opening video \"" + fileName + "\""); // 146
+	fileName.Upper();
+	fileName.Delete(".BIK", 0);
+	auto ret = zCBinkPlayer::OpenVideo(fileName + ".BIK");
+	if ( !ret && zrenderer ) {
+		// TODO
+	}
+}
--- decomp/code/Video/zCBinkPlayer.h
+++ decomp/code/Video/zCBinkPlayer.h
@@ -44,42 +44,23 @@ struct zCBinkPlayer : zCVideoPlayer {
 		return type;
 	} 
 
-	virtual void OpenVideo(zSTRING);
-	virtual void CloseVideo();
-	virtual void PlayInit(int);
-	void PlayFrame() override
+	int OpenVideo(zSTRING fileName) override;
+	int CloseVideo() override
 	{
-		if ( IsPlaying() ) {
-			zCVideoPlayer::PlayFrame();
-			if ( IsPlaying() ) {
-				BinkDoFrame(binkHandle);
-				PlayGotoNextFrame();
-				PlayWaitNextFrame();
-				zTRndSurfaceDesc surf;
-				if ( zrenderer->Vid_Lock(surf) ) {
-					if ( pixelFormat < 0 ) {
-						zWARNING("B: VP: Unknown pixel-format"); // 322
-					} else {
-						BinkCopyToBuffer(binkHandle, surf.surface, surf.surfPitch, surf.height, sizex, sizey, pixelFormat);
-					}
-					zrenderer->Vid_Unlock();
-					if ( unk ) {
-						zrenderer->Vid_Blit(0, &srcRect, &dstRect);
-						byte_8923CC = 1;
-					}
-				}
-    }
-    result = 1;
-  }
-  else
-  {
-    result = 0;
-  }
-  return result;
-}
+		if ( binkHandle ) {
+			zINFO_B(4, "B: VP: Closing Videofile: \"" + GetVideoFilename() +  "\""); // 128
+			BinkClose(binkHandle);
+			binkHandle = 0;
+			int ret = zCVideoPlayer::CloseVideo(&this->vt);
+			zINFO_C(4, "B: VP: Closing Videofile finished"); // 134
+			return ret;
 		}
+		return 0;
 	}
-	virtual void PlayDeinit();
+
+	int PlayInit(int frameNr) override;
+	int PlayFrame() override;
+	int PlayDeinit() override;
 	int Pause() override
 	{
 		BinkPause(binkHandle, 1);
@@ -147,3 +128,178 @@ private:
 	RECT srcRect;
 	RECT dstRect;
 };
+
+int zCBinkPlayer::OpenVideo(zSTRING fileName)
+{
+
+	zFILE_FILE file(fileName);
+	if ( file.GetExt().IsEmpty() )
+		file.SetExt("BIK");
+	if ( !file.Exists() ) {
+		zWARNING("B: VP: File \"" + file.GetFile() + "\" does not exist."); // 74
+		return 0;
+	}
+
+	zINFO_B(4, "B: VP: Open Videofile: \"" + file.GetFile() "\""); // 78
+
+	if (auto mss = dynamic_cast<zCSndSys_MSS*>(zsound)) {
+		if ( auto drv = zCSndSys_MSS::GetDigitalDriverHandle() ) {
+			BinkSetSoundSystem(BinkOpenMiles, drv);
+			BinkSetSoundOnOff(binkHandle, 1);
+			BinkSetVolume(binkHandle, 0, 0x10000);
+		}
+	}
+
+	fileName = file.GetFullPath();
+	binkHandle = BinkOpen(fileName.Data(), 0);
+	if ( binkHandle ) {
+		zCVideoPlayer::OpenVideo(fileName);
+		zINFO(4, "B: VP: Filesize: "_s + binkHandle->Size / 1024.0 + " kb"); //109,
+		zINFO(4, "B: VP: Resolution: "_s + binkHandle->decompheight + "x" + binkHandle->decompwidth); //110,
+		zINFO(4, "B: VP: Framecount: "_s + binkHandle->Frames); // 111
+		zINFO(4, "B: VP: Framerate: "_s + binkHandle->fileframerate + " fps"); // 112,
+		zINFO(4, "B: VP: Trackcount (Sound): " + binkHandle->NumTracks); // 113
+	} else {
+		zWARNING( "B: VP: Could not open videofile \"" + fileName + "\""); // 117
+	}
+
+	zINFO_C("B: VP: Open Videofile finished"); // 119
+	return binkHandle != 0;
+}
+
+int zCBinkPlayer::PlayInit(int frameNr)
+{
+	if ( !binkHandle )
+		return 0;
+	zINFO_B(3,"B: VP: Initialize Videoplay: \"" + GetVideoFilename() "\""); // 143,
+	if ( !zCVideoPlayer::PlayInit(frameNr) ) {
+		CloseVideo();
+		return 0;
+	}
+
+	if ( frameNr > 0 )
+		BinkGoto(binkHandle, frameNr, 0);
+
+	zTRndSurfaceDesc surf;
+	pixelFormat = -1;
+	sizex = -1;
+	sizey = -1;
+
+	int doPlay = 0;
+
+	if ( zrenderer->Vid_Lock(surf) ) {
+		zrenderer->Vid_Unlock();
+		pixelFormat = GetPixelFormat(surf);
+		if ( pixelFormat >= 0 ) {
+			zINFO(4,"B: VP: Pixelformat: "_s + pixelFormat); //174,
+		} else {
+			zINFO(4,"B: VP: Unknown pixelformat."); // 173
+		}
+
+		if ( pixelFormat >= 0 ) {
+			doPlay = 1;
+			if ( binkHandle->Height > surf.height ||
+			     binkHandle->Width > surf.width ) {
+
+				zWARNING("B: VP: Video won't play because of low resolution: "_s + surf.width + "x" + surf.height); // 190
+				doPlay = 0;
+			}
+		}
+	} else {
+		zFAULT("B: VP: Renderer is locked."); // 165
+	}
+
+	auto scaleVideos = zoptions->ReadBool(zOPT_SEC_GAME, "scaleVideos", 1);
+	if ( doPlay ) {
+		float hratio = surf.height / binkHandle->Height;
+		float wratio = surf.width / binkHandle->Width;
+		float scale = 0.0;
+		if ( wratio >= hratio )
+			scale = hratio;
+		else
+			scale = wratio;
+
+		if (!scaleVideos)
+			scale = 1.0;
+
+		sizex = (surf.height - binkHandle->Height * scale) * 0.5;
+		sizey = (surf.width  - binkHandle->Width * scale)  * 0.5;
+		if ( sizex < 0 || sizey < 0 ) {
+			doPlay = false;
+		} else {
+			srcRect.left = 0;
+			srcRect.top = 0;
+			srcRect.right = binkHandle->Width + 2 * sizex;
+			srcRect.bottom = binkHandle->Height + 2 * sizey;
+			destRect.left = 0;
+			destRect.top = 0;
+			destRect.right = surf.width;
+			destRect.bottom = surf.height;
+			zINFO(4,"B: VP: Scale-Factor: "_s + scale); // 225
+			zINFO(4,"B: VP: Video-Offset: "_s + sizex + "/" + sizey); // 226
+			if ( fullScreen ) {
+				fsTexture = "default.tga";
+				Delete(view);
+				view = new zCView(0, 0, 0x2000, 0x2000, 2);
+				view->SetColor(GFX_BLACK);
+				view->InsertBack(fsTexture);
+				view->InsertItem(screen, view, 0);
+				view->Render(screen);
+				zrenderer->Vid_Blit(1, srcRect, dstRect);
+				view->Render(screen);
+				zrenderer->Vid_Blit(1, srcRect, dstRect);
+			}
+		}
+	}
+
+	byte_8923CC = 1;
+
+	if ( !doPlay )
+		CloseVideo();
+	zINFO_C(3,"B: VP: Initialize Videoplay finished"); // 256
+	return doPlay;
+}
+
+int zCBinkPlayer::PlayDeinit()
+{
+	zINFO_B(3,"B: VP: Deinitialize Videoplay: \"" + GetVideoFilename() +  "\""); // 262
+	if ( binkHandle ) {
+		zINFO(4, "B: VP: Frames played: "_s + binkHandle->playedframes); // 266
+		zINFO(4, "B: VP: Skipped blits: "_s + binkHandle->skippedblits); // 267
+		zINFO(4, "B: VP: Memory used: "_s + binkHandle->totalmem / 1024.0 + " kb"); // 271
+		zINFO(4, "B: VP: Time open: "_s + binkHandle->timeopen  + " sec"); // 272
+		zINFO(4, "B: VP: Time blit: "_s + binkHandle->timeblit  + " sec"); // 273
+	}
+	int ret = zCVideoPlayer::PlayDeinit();
+	Delete(view);
+
+	zINFO_C(3, "B: VP: Deinitialize Videoplay finished"); // 280
+	return ret;
+}
+
+int zBinkPlayer::PlayFrame()
+{
+	if ( IsPlaying() ) {
+		zCVideoPlayer::PlayFrame();
+		if ( IsPlaying() ) {
+			BinkDoFrame(binkHandle);
+			PlayGotoNextFrame();
+			PlayWaitNextFrame();
+			zTRndSurfaceDesc surf;
+			if ( zrenderer->Vid_Lock(surf) ) {
+				if ( pixelFormat < 0 ) {
+					zWARNING("B: VP: Unknown pixel-format"); // 322
+				} else {
+					BinkCopyToBuffer(binkHandle, surf.surface, surf.surfPitch, surf.height, sizex, sizey, pixelFormat);
+				}
+				zrenderer->Vid_Unlock();
+				if ( unk ) {
+					zrenderer->Vid_Blit(0, &srcRect, &dstRect);
+					byte_8923CC = 1;
+				}
+			}
+		}
+		return 1;
+	}
+	return 0;
+}

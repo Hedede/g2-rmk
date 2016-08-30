36c95b112bb0ae65db57c313aee06fb43a476a54
Hudd <hededrk@gmail.com>
2016-08-30 16:07:13 +0300

    decomp: Remove leftover oCMenu_ChgKeys

--- decomp/code/Menu/oCMenu_ChgKeys.h
+++
@@ -1,8 +0,0 @@
-class oCMenu_ChgKeys : public zCMenu {
-public:
-	virtual void HandleEvent(int);
-	virtual void Leave();
-	virtual ~oCMenu_ChgKeys();
-	virtual void ScreenInit();
-	virtual void ExecCommand(zSTRING const&);
-};

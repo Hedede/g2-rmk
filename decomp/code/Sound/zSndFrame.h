74ef5aaa0e235f43abe53ff11405e41359bc49f2
Hudd <hededrk@gmail.com>
2016-09-01 01:27:40 +0300

    decomp: Add zCSndFrame

---
+++ decomp/code/Sound/zSndFrame.h
@@ -0,0 +1,15 @@
+struct zCSndFrame {
+	zSTRING file;
+	int pitchOff;
+	int pitchVar;
+	int vol;
+	int loop;
+	int loopStartOffset;
+	int loopEndOffset;
+	float reverbLevel;
+	zSTRING pfxName;
+	int unk2;
+	zSTRING str3;
+	int unk[4];
+};
+

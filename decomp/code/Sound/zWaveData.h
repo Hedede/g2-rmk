80cd138a46226ac022d8fa0cb1055252f7ab0267
Hudd <hededrk@gmail.com>
2016-09-17 10:14:56 +0300

    decomp: Decompile zCWaveData

--- decomp/code/Sound/zWaveData.h
+++ decomp/code/Sound/zWaveData.h
@@ -1,6 +1,7 @@
 //_carsten\\zSndMss.cpp
 struct zCWaveData {
 	zCWaveData() { Init(); }
+	zCWaveData(zSTRING& name) : zCWaveData() { this->name = name }
 	~zCWaveData()
 	{
 		if ( refCtr ) {
@@ -20,6 +21,13 @@ struct zCWaveData {
 	zSTRING GetName() const
 	{
 		return name;
+	}
+
+	int GetSize()
+	{
+		if (sizeBytes == 0)
+			CacheIn();
+		return sizeBytes;
 	}
 
 	void CacheOut()
@@ -91,3 +99,72 @@ private:
 	int soundOwnedByAIL;
 };
 
+
+void zCWaveData::CacheIn()
+{
+	if ( !soundData ) {
+		zPATH path(name);
+		auto dir = zoptions->GetDirString(DIR_SOUND);
+
+		if (path.file->SearchFile(name, dir, 1) == 5003) {
+			zWARNING("C: zSndMSS.h(zCWaveData::zCWaveData): Wave not found: " + name); // 3947,
+			auto dir = zoptions->GetDirString(DIR_SOUND);
+
+			if (path.file->SearchFile(MSS_DEF_WAVE, dir, 1))
+				zFATAL("C: zSndMSS.h(zCWaveData::zCWaveData): Wave not found: " + MSS_DEF_WAVE); // 3948
+		}
+
+		auto zfile = zfactory->CreateZFile(path.GetPath());
+
+		if ( zfile->Open2(0) ) {
+			zFAULT("C: zCWaveData::CacheIn(): could not open file: " + path.GetPath()); // 3986
+		} else {
+			sizeBytes = zfile->Size();
+			soundData = new char[sizeBytes];
+			zfile->Read(soundData, sizeBytes);
+			zfile->Close();
+			if ( !waveInfo ) {
+				waveInfo = new _AILSOUNDINFO;
+				AIL_WAV_info(soundData, waveInfo);
+			}
+			if ( !sizeBytes )
+				CacheIn();
+
+			if ( AIL_file_type(soundData, sizeBytes) == 2 ) {
+				zWARNING(9,"C: SND: zCWaveData::CacheIn(): decompressing ADPCM Wave: " + GetName()); // 3972,
+
+				char* ptr;
+				int size;
+				if ( !AIL_decompress_ADPCM(waveInfo, &ptr, &sizw) ) {
+					zFATAL("C: zCWaveData::CacheIn(): could not decompress ADPCM file " + GetName()); // 3976,
+				}
+
+				delete[] soundData;
+				soundData = ptr;
+				sizeBytes = size;
+				AIL_WAV_info(soundData, waveInfo);
+				soundOwnedByAIL = 1;
+			}
+		}
+		delete zfile;
+
+		sndMemUsed += sizeBytes;
+	}
+
+	if ( playingTime != 0.0 ) {
+		++usedCtr;
+		return;
+	}
+
+	auto actSnd = zCActiveSnd::AllocNextFreeSnd();
+	if ( actSnd->RequestChannel() ) {
+		AIL_set_sample_file(actSnd->sampleHandle, soundData, 0);
+		AIL_sample_ms_position(actSnd->sampleHandle, &playingTime, 0);
+		AIL_release_sample_handle(actSnd->sampleHandle1);
+		actSnd->Release();
+		++usedCtr;
+		return;
+	}
+
+	zWARNING("C: SND: (zCWaveData::CacheIn()) could not allocate sample handle for wave duration evaluation (Name:" + GetName() +  ")"); // 4003,
+}
--- decomp/code/Sound/zWavePool.h
+++ decomp/code/Sound/zWavePool.h
@@ -9,7 +9,26 @@ struct zCWavePool {
 	zCWavePool()
 	{
 		waveList.Compare = CompareWaves;
-		lastData = new zCWaveData;
+		tmpWave = new zCWaveData;
+	}
+
+	zCWaveData* CacheIn(zSTRING& fileName)
+	{
+		critSect.Lock(-1);
+
+		tmpWave->name = fileName;
+
+		zCWaveData* wave;
+		auto idx = waveList.Search(tmpWave);
+		if (idx == -1) {
+			wave = new zCWaveData(fileName);
+			waveList.InsertSort(wave);
+		}
+
+		wave->CacheIn();
+
+		critSect.Unlock();
+		return wave;
 	}
 
 	void CacheIn(zCWaveData* wave)
@@ -47,16 +66,18 @@ struct zCWavePool {
 		critSect.Unlock();
 	}
 
-	// Present in G1demo, not present in G2
 	void DebugInfo();
+	// Used in G1demo, but unused in G2
 	void DebugInfo2();
+
+	// seems to be bullshit and not used
+	void NewFrame();
 
 private:
 	zCArraySort<zCWaveData*> waveList;
-	zCWaveData *lastData;
+	zCWaveData *tmpWave;
 	zCCriticalSection critSect;
 
-	void *wtf = 0;
-	int i1    = 0;
-	int i2    = 0;
+	// used by NewFrame(), i.e. unused
+	zCArray<zCWaveData*> wtf;
 };

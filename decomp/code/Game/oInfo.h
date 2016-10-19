dd63cff4c02f1ef9f711c8504a468aa85b9a8e84
Hudd <haddayn@gmail.com>
2016-10-19 04:42:29 +0300

    decomp: Decompile oCInfo

--- decomp/code/Game/oInfo.h
+++ decomp/code/Game/oInfo.h
@@ -1,20 +1,20 @@
-class oCInfoChoice
-{
-    zSTRING Text;
-    int Function;  //symbolindex    
+//_Ulf/oInfo.cpp
+class oCInfoChoice {
+	~oCInfoChoice() = default;
+	zSTRING Text;
+	int Function;  //symbolindex
 };
 
-/* Groﬂe Vorsicht!
+/* Gro√üe Vorsicht!
 
-Im Gegensatz zu C_NPC und C_ITEM zeigt eine C_INFO nicht auf den vergrˆﬂerten
-Speicherbereich, sondern tats‰chlich auf sich selbst.
+Im Gegensatz zu C_NPC und C_ITEM zeigt eine C_INFO nicht auf den vergr√∂√üerten
+Speicherbereich, sondern tats√§chlich auf sich selbst.
 MEM_InstGetOffset liefert also, auf eine C_INFO angewendet NICHT die Adresse
-des umrahmenden oCInfo Objekts sondern die Adresse der enthaltenen C_INFO zur¸ck.
-Um an die oCInfo zu kommen m¸ssen 4 + 4 + 20 = 28 abgezogen werden! */
+des umrahmenden oCInfo Objekts sondern die Adresse der enthaltenen C_INFO zur√ºck.
+Um an die oCInfo zu kommen m√ºssen 4 + 4 + 20 = 28 abgezogen werden! */
 
 class oCInfo {
-public:
-	oCInfo();
+	oCInfo() = default;
 	virtual ~oCInfo();
 	virtual void Archive(zCArchiver& arc)
 	{
@@ -33,13 +33,76 @@ public:
 		arc.ReadString("InstName", name);
 
 		int instance = zparser.GetIndex(name);
-		if ( !zparser.CreateInstance(instance, &parserdef) )
+		if ( !zparser.CreateInstance(instance, &par) )
 			zWARNING("Info instance not found.");
 	}
 
 	virtual void RestoreParserInstance()
 	{
 		zparser.SetInstance(instance, &parserdef);
+	}
+
+	zSTRING& GetText { return par.description; }
+
+	void* GetDataAdr() { return &par; }
+
+	int GetConditionFunc() const { return par.conditions; }
+	int GetNpcID() const { return par.npc; }
+	int GetInstance() const { return instance; }
+
+	int GetDataSize() const { return sizeof(par); }
+
+	void SetTold(zBOOL b) { told = b; }
+	bool WasTold() const
+	{
+		if (par.permanent)
+			return false;
+		return told;
+	}
+
+	int InfoConditions()
+	{
+		return *zparser.CallFunc(par.conditions);
+	}
+
+	void Info()
+	{
+		zparser.CallFunc(par.information);
+		SetTold(true);
+	}
+
+	void RemoveAllChoices()
+	{
+		for (int i = 0, e = listChoices.GetNumInList(); i < e; ++i)
+			listChoices.RemoveIndex(i);
+		listChoices.DeleteList();
+	}
+
+	void AddChoice(zSTRING strText, int nFunc)
+	{
+		listChoices.Insert({strText, nFunc});
+	}
+
+	void RemoveChoice(zSTRING strText)
+	{
+		// dontcare
+		int idx = listChoices.Search(strText);
+		listChoices.Remove(idx);
+	}
+
+	void SetInstance(int nr)
+	{
+		int typ;
+		instance = nr;
+		name = zparser.GetSymbolInfo(instance, &typ, &nr);
+	}
+
+	int DoCheck()
+	{
+		if ( par.npc <= 0 ) {
+			zWARNING("U: INF: Skriptinfo:npc is not/wrong initialised:" + par.description + " " + par.information); // 465
+		}
+		return 1;
 	}
 
 private:
@@ -49,21 +112,21 @@ private:
 
 	//Wohlbekannt:
 	struct TParserDef {
-		int     npc;
-		int     nr;
-		zBOOL   important;
-		int     conditions;
-		int     information;
+		int     npc = 0;
+		int     nr = -1;
+		zBOOL   important = false;
+		int     conditions  = -1;
+		int     information = -1;
 		zSTRING description;
-		zBOOL   trade;
-		zBOOL   permanent;
-	} parserdef;
+		zBOOL   trade     = false;
+		zBOOL   permanent = false;
+	} par;
 
 	//schonmal abgespielt (das was Npc_KnowsInfo abfragt)
-	zBOOL told;
+	zBOOL told = false;
 
 	//symbolindex der oCInfo im Parser.
-	int instance;
+	int instance = -1;
 
 	//Choices:
 	zCList< oCInfoChoice > listChoices;

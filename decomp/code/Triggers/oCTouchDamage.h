36461f8a54bec05804226b24c8b9b37f1392cf0d
Hudd <haddayn@gmail.com>
2016-10-19 04:58:53 +0300

    decomp: Add GetDamageTypeArcEnum

--- decomp/code/Triggers/oCTouchDamage.h
+++ decomp/code/Triggers/oCTouchDamage.h
@@ -3,5 +3,19 @@ class oCTouchDamage : zCTouchDamage {
 public:
 	virtual ~oCTouchDamage() = default;
 
-	virtual void GetDamageTypeArcEnum(ulong);
+	const char* GetDamageTypeArcEnum(unsigned long idx) override
+	{
+		switch ( idx ) {
+		case DAM_INDEX_BARRIER: return "Barrier";
+		case DAM_INDEX_BLUNT:   return "Blunt";
+		case DAM_INDEX_EDGE:    return "Edge";
+		case DAM_INDEX_FIRE:    return "Fire";
+		case DAM_INDEX_FLY:     return "Fly";
+		case DAM_INDEX_MAGIC:   return "Magic";
+		case DAM_INDEX_POINT:   return "Point";
+		case DAM_INDEX_FALL:    return "Fall";
+		}
+		return nullptr;
+	}
+
 };

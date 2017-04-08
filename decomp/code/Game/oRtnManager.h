d7f73bde80634f720e9bc6b30482e4d7f5d7edd4
Hudd <haddayn@gmail.com>
2017-04-08 19:08:58 +0300

    Add more of oCRtnManager code

--- decomp/code/Game/oRtnManager.h
+++ decomp/code/Game/oRtnManager.h
@@ -42,12 +42,12 @@ struct oCRtnEntry {
 	int min_end = 0;
 	int state = 0;
 	zSTRING waypoint;
-	int unk0 = -1;
+	int instance = -1;
 	int unk1 = 0;
 	oCNpc* npc = nullptr;
 	oCRtnCutscene* cs = nullptr;
-	int unk4 = 0;
-	int unk[2];
+	int overlay = 0;
+//0x3C
 };
 
 struct oCRtnCutscene {
@@ -75,12 +75,122 @@ struct oCRtnCutscene {
 };
 
 struct oCRtnManager {
+	oCRtnManager()
+	{
+		for(int i = 0; i < 3; ++i) {
+			rtn0[i] = 0;
+			rtn0[i + 3] = 0;
+		}
+		rtnEntries.compare  = oCRtnManager::Sort_Routine;
+		wayBoxes[0].compare = oCRtnManager::Sort_WayBoxX;
+		wayBoxes[1].compare = oCRtnManager::Sort_WayBoxY;
+		wayBoxes[2].compare = oCRtnManager::Sort_WayBoxZ;
+	}
+	~oCRtnManager() = default;
+
+	void SetWorld(zCWorld* wld)
+	{
+		world = wld;
+	}
+
+	int GetNumOfWayBoxes() const
+	{
+		return wayBoxes[0].GetNumInList();
+	}
+
+	void Insert(oCNpc *npc, oCRtnEntry *rtn)
+	{
+		if ( rtn ) {
+			if ( npc ) {
+				rtn->npc = npc;
+				rtnEntries.InsertSort(rtn);
+			}
+		}
+	}
+
+	void DrawWayBoxes();
+
+	struct TRtn_WayBox { zTBBox3D bbox; };
+	struct TRtn_WayBoxLimit {
+		int type;
+		zTBBox3D bbox;
+		float limit;
+		oCNpc *npc;
+	};
+	
+private:
+	static int Sort_WayBoxX(TRtn_WayBox *a, TRtn_WayBox *b)
+	{
+		return (a->bbox.mins[0] > b->bbox.mins[0]) ? 1 : -1;
+	}
+	static int Sort_WayBoxY(TRtn_WayBox *a, TRtn_WayBox *b)
+	{
+		return (a->bbox.mins[1] > b->bbox.mins[1]) ? 1 : -1;
+	}
+	static int Sort_WayBoxZ(TRtn_WayBox *a, TRtn_WayBox *b)
+	{
+		return (a->bbox.mins[2] > b->bbox.mins[2]) ? 1 : -1;
+	}
+	static int Sort_WayBoxLimit(TRtn_WayBoxLimit *a, TRtn_WayBoxLimit *b)
+	{
+		return (a->limit > b->limit[2]) ? 1 : -1;
+	}
+
+private:
 	char rtn0[24];
-	zCWorld *world;
-	zCListSort *nextEntry;
-	zCListSort rtnEntries;
-	char rtn1[8];
-	zCListSort wayBoxes[3];
-	zCArraySort wayBoxLimits[3];
+	zCWorld *world = nullptr;
+	zCListSort<oCRtnEntry> *nextEntry = nullptr;
+	zCListSort<oCRtnEntry> rtnEntries;
+	zCListSort<TRtn_WayBox> wayBoxes[3];
+	zCList<oCNoc*> npcs;
+	zCArraySort<TRtn_WayBoxLimit*> wayBoxLimits[3];
 //0x88
 };
+
+// _Ulf/oRoutine.cpp
+void oCRtnManager::InitWayBoxes()
+{
+	auto npcs = ogame->GetGameWorld()->voblist_npcs;
+	for ( oCNpc* npc : npcs ) {
+		if (!npc->IsDead() || npc->IsAPlayer())
+			CreateWayBoxes(npc);
+	}
+}
+
+void oCRtnManager::DrawWayBoxes()
+{
+	for ( auto box : wayBoxes ) {
+		zCOLOR color;
+		// don't remeber, is it rgba or bgra?
+		color[2] = 0;
+		color[1] = 0;
+		color[0] = -1;
+		color[3] = -1;
+		box.Draw(color);
+	}
+}
+
+// private
+void oCRtnManager::InsertWaybox(oCNpc *npc, zVEC3& mins, zVEC3& maxs)
+{
+	// very messy with stupid compiler shortcut pointer manipulation
+	// "mins[i]" is compiled as as "maxs[i] + (mins - maxs)" (all pointers)
+	int i = 3;
+	while (i --> 0) {
+		auto min = new TRtn_WayBoxLimit;
+		min->type = 1;
+		min->npc = npc;
+		min->bbox.mins = mins;
+		min->bbox.maxs = maxs;
+		min->limit = mins[i];
+		wayBoxLimits[i].InsertSort(min);
+
+		auto max = new TRtn_WayBoxLimit;
+		max->type = 0;
+		max->npc = npc;
+		max->bbox.mins = mins;
+		max->bbox.maxs = maxs;
+		max->limit = maxs[i];
+		wayBoxLimits[i].InsertSort(max);
+	}
+}

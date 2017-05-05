c7f8adc389b9ad10679b5d8550253262c3558921
Hudd <haddayn@gmail.com>
2017-05-05 09:02:52 +0300

    Decompile a bit of zCMeshSoftSkin

--- decomp/code/Render/zMeshSoftSkin.h
+++ decomp/code/Render/zMeshSoftSkin.h
@@ -1,6 +1,90 @@
-class zCMeshSoftSkin {
- virtual void _GetClassDef();
- virtual void ~zCMeshSoftSkin(uint);
- virtual void Load(zCFileBIN &);
- virtual void Save(zCFileBIN &);
+// _Dieter/zProgMeshProto.cpp
+class zCMeshSoftSkin : public zCProgMeshProto {
+	Z_OBJECT(zCMeshSoftSkin);
+public:
+	zCMeshSoftSkin() = default;
+	void ~zCMeshSoftSkin()
+	{
+		DeleteVertWeightStream();
+		DeleteNodeOBBList();
+	}
+
+	void Load(zCFileBIN &) override;
+	void Save(zCFileBIN &) override;
+
+	static short GetFileVersion() { return 4; }
+	static int GetLowestLODNumPolys() { return lowestLodNumPolys; }
+
+#pragma pack(push, 1)
+	struct zTWeightEntry {
+		float Weight;
+		zVEC3 VertexPosition;
+		uint8_t NodeIndex;
+	};
+#pragma pack(pop)
+
+	struct zTNodeWedgeNormal {
+		float data[4];
+	};
+
+	void ResetVertWeightIterator()
+	{
+		weightIter = vertWeightStream;
+	}
+
+	void DeleteVertWeightStream()
+	{
+		delete[] vertWeightStream;
+		vertWeightStream = 0;
+		weightIter = 0;
+	}
+
+	void AllocVertWeightStream(int numSVerts, int numWeightEntrys)
+	{
+		DeleteVertWeightStream();
+		auto size = sizeof(zTWeightEntry) * numWeightEntrys + sizeof(int) * numSVerts;
+		vertWeightStream = new char[size];
+		ResetVertWeightIterator();
+	}
+
+	void AddNumWeightEntry(const int& numSVert)
+	{
+		*(int*)weightIter = numSVert;
+		++(int*)weightIter;
+	}
+
+	void AddWeightEntry(zTWeightEntry const& entry)
+	{
+		*(zTWeightEntry*)weightIter = entry;
+		++(zTWeightEntry*)weightIter;
+	}
+
+	int GetVertWeightStreamSize()
+	{
+		ResetVertWeightIterator();
+		int result = 0;
+		for (int i = vertList.size; i > 0; --i) {
+			int num = *(int*)weightIter;
+			int size = sizeof(int) + num * sizeof(zTWeightEntry);
+
+			result     += size;
+			weightIter += size;
+		}
+		return result;
+	}
+
+	void DeleteNodeOBBList()
+	{
+		for (auto obb : nodeObbList)
+			delete obb;
+		nodeObbList.DeleteList();
+	}
+
+private:
+	zCArray<int>               nodeIndexList;
+	zCArray<zCOBBox3D*>        nodeObbList;
+	zCArray<zTNodeWedgeNormal> nodeWedgeNormals;
+
+	char *vertWeightStream = nullptr;
+	char *weightIter       = nullptr;
 };

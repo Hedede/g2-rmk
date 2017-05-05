aadcac97c3b45def85ee1e6110f7f99809723692
Hudd <haddayn@gmail.com>
2017-05-05 08:06:43 +0300

    Fully decompile oCTriggerScript

--- decomp/code/Triggers/oCTriggerScript.h
+++ decomp/code/Triggers/oCTriggerScript.h
@@ -1,19 +1,44 @@
+// _bert/oTrigger.cpp
 class oCTriggerScript : public zCTrigger {
 	Z_OBJECT(oCTriggerScript);
 public:
-	virtual void Archive(zCArchiver& arc);
+	oCTriggerScript() = default;
+	virtual ~oCTriggerScript() = default;
+
+	void Archive(zCArchiver& arc) override
 	{
 		zCTrigger::Archive(arc);
 		arc.WriteString("scriptFunc", scriptFunc);
 	}
-	virtual void Unarchive(zCArchiver& arc)
+	void Unarchive(zCArchiver& arc) override
 	{
 		zCTrigger::Unarchive(arc);
 		arc.ReadString("scriptFunc", scriptFunc);
 	}
-	virtual ~oCTriggerScript();
-	virtual void TriggerTarget(zCVob *);
-	virtual void UntriggerTarget(zCVob *);
+
+	void TriggerTarget(zCVob *vobInstigator) override
+	{
+		zCTrigger::TriggerTarget(vobInstigator);
+		zINFO(8, "B: TRIGGER: " + GetClassName() + " " + GetObjectName() + " triggered -> " + scriptFunc); // 157
+
+		zparser.SetInstance("SELF", dynamic_cast<oCNpc*>(vobInstigator));
+		zparser.SetInstance("OTHER", nullptr);
+		zparser.SetInstance("ITEM", nullptr);
+		zparser.CallFunc(scriptFunc);
+	}
+	void UntriggerTarget(zCVob* vobInstigator) override
+	{
+		zINFO(8, "B: TRIGGER: " + GetClassName() + " " + GetObjectName() + " untriggered -> " + scriptFunc); // 166
+		zCTrigger::UntriggerTarget(vobInstigator);
+	}
+
+	void SetScriptFunc(zSTRING const& name, int calls)
+	{
+		scriptFunc = name;
+		countCanBeActivated = calls;
+		numCanBeActivated = calls;
+	}
+
 private:
 	zSTRING scriptFunc;
 };

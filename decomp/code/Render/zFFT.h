bc4789b90eb8eff0149871400b32b512cf2c75a6
Hudd <haddayn@gmail.com>
2017-08-26 17:14:38 +0300

    decomp: Add more details on zCFFT

--- decomp/code/Render/zFFT.h
+++ decomp/code/Render/zFFT.h
@@ -1,7 +1,11 @@
-// This one is clearly made by other developers, as it doesn't use
-// zERROR (uses stdout instead) and other stuff made by original three
+// zCFFT from Gothic2 uses telecomm-FFT
 class zCFFT {
-	virtual ~zCFFT();
+	zCFFT() = default;
+	virtual ~zCFFT()
+	{
+		for (auto array : arrays)
+			FreeArray(array);
+	}
 
 	static void S_Init();
 
@@ -9,10 +13,17 @@ class zCFFT {
 	void SetFrameCtr(int ctr) { frameCtr = ctr; }
 
 	zComplex **arrays[3];
-	float speed;
-	float frameCtr;
-	float frameCtr2; // not sure what exactly
+	float speed   = 1.0;
+	int frameCtr  = 0;
+	int frameCtr2 = -1; // not sure what exactly
 };
+
+void zCFFT::S_SetFrameCtr(int ctr)
+{
+	s_fft.frameCtr  = ctr;
+	s_fft2.frameCtr = ctr;
+	s_fft3.frameCtr = ctr;
+}
 
 void zCFFT::S_Init()
 {
@@ -38,8 +49,24 @@ void zCFFT::CreateWaveMap(float h, zVEC3& vec)
 zComplex **zCFFT::CreateArray()
 {
 	zComplex** array = new zComplex*[32];
-	for (int i = 0; i < 32; ++i) {
+	for (int i = 0; i < 32; ++i)
 		array[i] = new zComplex[32] { 0, 0, 0, 0 };
-	}
 	return array;
 }
+
+void zCFFT::FreeArray(zComplex** array)
+{
+	for (int i = 0; i < 32; ++i)
+		delete array[i];
+	delete array;
+}
+
+#include "fourier.h"
+
+// this one is based on fft_float, but it was modified to take zComplex* instead of float*,float*
+void zCFFT::fft_float(unsigned NumSamples, int InverseTransform, zComplex* in, zComplex* out );
+
+int      zCFFT::IsPowerOfTwo( unsigned x );
+unsigned zCFFT::NumberOfBitsNeeded( unsigned PowerOfTwo );
+unsigned zCFFT::ReverseBits( unsigned index, unsigned NumBits );
+double   zCFFT::Index_to_frequency( unsigned NumSamples, unsigned Index );

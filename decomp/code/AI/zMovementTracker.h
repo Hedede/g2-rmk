54d5b9bdf8599e2b07d61106bca3c694f1ba4a02
Hudd <haddayn@gmail.com>
2019-01-18 21:15:09 +0300

    Do research on zMovementTracker fields

--- decomp/code/AI/zMovementTracker.h
+++ decomp/code/AI/zMovementTracker.h
@@ -4,6 +4,8 @@ struct zCPose {
 
 struct zCMovementTracker {
 
+	zVEC3& GetLastValidWayPoint(zTWayPoint const& type);
+
 private:
 	void CheckKeys() {}
 
@@ -14,14 +16,22 @@ private:
 	float unk00[3];
 	zVEC3 playerPos;
 	zVEC3 playerPos2;
-	char unk1[12];
+	zVEC3 __startPos;
 	zVEC3 __testPos;
 	zVEC3 __camPos2;
-	int unk2[16];
+	int unk2[13];
+	int __targetInertia;
+	int unkx1;
+	int unkx2;
 	zCPose __pose1;
 	zCPose __pose2;
 	zCPose __pose3;
-	int unk3[102];
+	zVEC3 waypointPosition[12];
+	int __wp_pos_idx[4];
+	float __wp_0305[4];
+	int unk3[32];
+	float __ringRot[4];
+	int unk3_[22];
 	zMAT4 __mat;
 	int unko4[32];
 	zMAT4 __mat2;
@@ -37,6 +47,70 @@ private:
 	oCNpc *player;
 	zCPathSearch *pathSearch;
 	zCAICamera *aiCam;
-	int unkoooo;
+	zCPathSearch *unkoooo;
 };
 
+
+zVEC3& zCMovementTracker::GetLastValidWayPoint(zTWayPoint const& type)
+{
+	// very repetitive, I suspect compiler might've unrolled this
+	int idx;
+	switch ( type ) {
+	case 0:
+	case 2:
+		idx = __wp_pos_idx[0];
+		if ( 0.3 * 0.5 >= __wp_0305[0] ) {
+			if ( idx > 1 )
+				return waypointPosition[idx-2];
+			if ( idx == 1 )
+				return waypointPosition[2];
+			return waypointPosition[1];
+		} else {
+			if ( idx > 0 ) {
+				return waypointPosition[idx-1];
+			return waypointPosition[2];
+		}
+	case 3:
+		idx = __wp_pos_idx[1];
+		if ( 0.3 * 0.5 >= __wp_0305[1] ) {
+			if ( idx > 1 )
+				return waypointPosition[idx+1];
+			if ( idx == 1 )
+				return waypointPosition[5];
+			return waypointPosition[4];
+		} else {
+			if ( idx > 0 ) {
+				return waypointPosition[idx+2];
+			return waypointPosition[5];
+		}
+	case 4:
+		idx = __wp_pos_idx[2];
+		if ( 0.3 * 0.5 >= __wp_0305[2] ) {
+			if ( idx > 1 )
+				return waypointPosition[idx+4];
+			if ( idx == 1 )
+				return waypointPosition[8];
+			return waypointPosition[7];
+		} else {
+			if ( idx > 0 ) {
+				return waypointPosition[idx+5];
+			return waypointPosition[8];
+		}
+		break;
+	case 1:
+		idx = __wp_pos_idx[3];
+		if ( 0.3 * 0.5 >= __wp_0305[3] ) {
+			if ( idx > 1 )
+				return waypointPosition[idx+7];
+			if ( v2 == 1 )
+				return waypointPosition[11];
+			return waypointPosition[10];
+		} else {
+			if ( idx > 0 ) {
+				return waypointPosition[idx+8];
+			return waypointPosition[11];
+		}
+	default:
+		return { 0, 0, 0 };
+	}
+}

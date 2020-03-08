2a57af02e11c2ce33afbbc71f6f4d92b1f3a3b15
Hudd <haddayn@gmail.com>
2020-03-08 14:57:44 +0300

    decomp: Decompile zCVobLevelCompo

--- decomp/code/Vobs/zCVobLevelCompo.h
+++ decomp/code/Vobs/zCVobLevelCompo.h
@@ -3,13 +3,23 @@ class zCVobLevelCompo : public zCVob {
 private:
 	static zBOOL unarcIgnoreVisuals;
 public:
-	virtual ~zCVobLevelCompo();
+	zCVobLevelCompo();
+	~zCVobLevelCompo() override = default;
 
-	virtual void Archive(zCArchiver& arc);
-	virtual void Unarchive(zCArchiver& arc);
-	virtual void SetVisual(zSTRING const &);
-	virtual void SetVisual(zCVisual*);
+	void Archive(zCArchiver& arc) override;
+	void Unarchive(zCArchiver& arc) override;
+	void SetVisual(zSTRING const& visualFileName) override;
+	void SetVisual(zCVisual* newVisual) override;
+
+private:
+	zBOOL HasIdentityTrafo() const;
 };
+
+zCVobLevelCompo::zCVobLevelCompo()
+{
+	SetCollDetStat(0);
+	SetCollDetDyn(0);
+}
 
 void zCVobLevelCompo::Archive(zCArchiver& arc);
 {
@@ -33,10 +43,41 @@ void zCVobLevelCompo::Unarchive(zCArchiver& arc)
 	if ( zCVob::s_ignoreVisuals )
 		SetVisual(0);
 
-	for (int i = 0; i < 4; ++i) {
-		if (trafoObjToWorld[i][i] > 1.001) {
-			zMAT4 identity = Alg_Identity3D();
-			SetTrafo(identity);
-		}
+	// was inlined
+	if (!HasIdentityTrafo())
+		SetTrafo( Alg_Identity3D() );
+}
+
+zBOOL zCVobLevelCompo::HasIdentityTrafo() const
+{
+	if ( trafoObjToWorld[0][3] > 0.001)
+		return 0;
+
+	for (int i = 0; i < 4; ++i)
+	{
+		if (trafoObjToWorld[i][i] > 1.001)
+			return 0;
 	}
+
+	return 1;
+}
+
+void zCVobLevelCompo::SetVisual(zCVobLevelCompo *this, const struct zSTRING *visualFileName)
+{
+	auto matUsage = zCMaterial::s_matUsageDefault;
+	zCMaterial::s_matUsageDefault = 0;
+	zCVob::SetVisual(visualFileName);
+	zCVob::SetCollDetStat(0);
+	zCVob::SetCollDetDyn(0);
+	zCMaterial::s_matUsageDefault = matUsage;
+}
+
+void zCVobLevelCompo::SetVisual(zCVobLevelCompo *this, struct zCVisual *newVisual)
+{
+	auto matUsage = zCMaterial::s_matUsageDefault;
+	zCMaterial::s_matUsageDefault = 0;
+	zCVob::SetVisual(newVisual);
+	zCVob::SetCollDetStat(0);
+	zCVob::SetCollDetDyn(0);
+	zCMaterial::s_matUsageDefault = matUsage;
 }
